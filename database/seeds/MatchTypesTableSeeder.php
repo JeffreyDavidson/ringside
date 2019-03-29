168c6f9a619b5b6b5461c573aed84623778b2896
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-03-28 22:25:37 -0400

    Finish match types

--- database/seeds/MatchTypesTableSeeder.php
+++ database/seeds/MatchTypesTableSeeder.php
@@ -17,14 +17,14 @@ class MatchTypesTableSeeder extends Seeder
         MatchType::create(['name' => 'Triple Threat', 'slug' => 'triple']);
         MatchType::create(['name' => 'Triangle', 'slug' => 'triangle']);
         MatchType::create(['name' => 'Fatal 4 Way', 'slug' => 'fatal4way']);
-        MatchType::create(['name' => '6 Tag Team', 'slug' => 'tagteam']);
-        MatchType::create(['name' => '8 Tag Team', 'slug' => 'tagteam']);
-        MatchType::create(['name' => '10 Tag Team', 'slug' => 'tagteam']);
-        MatchType::create(['name' => 'Two On One Handicap Match', 'slug' => '21handicap']);
-        MatchType::create(['name' => 'Three On Two Handicap Match', 'slug' => '32handicap']);
-        MatchType::create(['name' => 'Battle Royal Match', 'slug' => 'battleroyal']);
-        MatchType::create(['name' => 'Royal Rumble Match', 'slug' => 'royalrumble']);
-        MatchType::create(['name' => 'Tornado Tag Team Match', 'slug' => 'tornadotag']);
-        MatchType::create(['name' => 'Gauntlet Match', 'slug' => 'gauntlet']);
+        MatchType::create(['name' => '6 Man Tag Team', 'slug' => '6man']);
+        MatchType::create(['name' => '8 Man Tag Team', 'slug' => '8man']);
+        MatchType::create(['name' => '10 Man Tag Team', 'slug' => '10man']);
+        MatchType::create(['name' => 'Two On One Handicap', 'slug' => '21handicap']);
+        MatchType::create(['name' => 'Three On Two Handicap', 'slug' => '32handicap']);
+        MatchType::create(['name' => 'Battle Royal', 'slug' => 'battleroyal']);
+        MatchType::create(['name' => 'Royal Rumble', 'slug' => 'royalrumble']);
+        MatchType::create(['name' => 'Tornado Tag Team', 'slug' => 'tornadotag']);
+        MatchType::create(['name' => 'Gauntlet', 'slug' => 'gauntlet']);
     }
 }
