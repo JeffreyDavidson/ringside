40a2d24cdc2091a74e61f702232b757505f65a45
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-05-08 12:41:59 -0400

    Putting together webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,25 +11,51 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.scripts(["resources/vendors/jquery/dist/jquery.js"], "public/js/vendors.js")
-    .styles(
-        [
-            "resources/css/theme/style.bundle.css",
-            "resources/css/theme/skins/header/base/light.css",
-            "resources/css/theme/skins/header/menu/light.css",
-            "resources/css/theme/skins/brand/dark.css",
-            "resources/css/theme/skins/aside/dark.css"
-        ],
-        "public/css/theme.css"
-    )
-    .scripts(["resources/js/scripts.bundle.js"], "public/js/app.js")
-    .styles(
-        [
-            "resources/vendors/flaticon/flaticon.css",
-            "resources/vendors/flaticon2/flaticon.css"
-        ],
-        "public/css/vendors.css"
-    )
-    .copyDirectory("resources/vendors/flaticon/font", "public/css/font")
-    .copyDirectory("resources/vendors/flaticon2/font", "public/css/font")
-    .sass("resources/sass/app.scss", "public/css");
+mix.styles(
+    [
+        "resources/css/theme/style.bundle.css",
+        "resources/css/theme/skins/header/base/light.css",
+        "resources/css/theme/skins/header/menu/light.css",
+        "resources/css/theme/skins/brand/dark.css",
+        "resources/css/theme/skins/aside/dark.css"
+    ],
+    "public/css/theme.css"
+);
+mix.extract([
+    "jquery",
+    "popper.js",
+    "bootstrap",
+    "js-cookie",
+    "moment",
+    "tooltip.js",
+    "perfect-scrollbar",
+    "sticky-js",
+    "wnumb",
+    "datatables"
+]);
+mix.styles(
+    [
+        "resources/vendors/flaticon/flaticon.css",
+        "resources/vendors/flaticon2/flaticon.css"
+    ],
+    "public/css/fonts.css"
+);
+mix.sass("resources/sass/app.scss", "public/css");
+
+/************************************************************************
+ * Copy Static Assets
+ ************************************************************************/
+mix.copyDirectory("resources/vendors/flaticon/font", "public/css/font");
+mix.copyDirectory("resources/vendors/flaticon2/font", "public/css/font");
+
+/************************************************************************
+ * Mix Configuration
+ * * We aren't keen on the "Build successful" notifications, so disable
+ *   them
+ * * We load in our webpack.config.js, which just contains aliases so
+ *   PHPstorm reads them
+ ************************************************************************/
+mix.disableSuccessNotifications();
+mix.sourceMaps(false, "inline-source-map");
+
+mix.js(["resources/js/scripts.bundle.js"], "public/js/app.js");
