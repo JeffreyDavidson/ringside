7e82e56f49707678b59fdc9100d3d0106f29bb22
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-06-19 19:48:12 -0400

    Working on venues list page

--- resources/js/entries/venues/index.js
+++ resources/js/entries/venues/index.js
@@ -7,15 +7,19 @@ var KTAppVenueListDatatable = (function() {
 
     // init
     var init = function() {
-        // init the datatables. Learn more: https://keenthemes.com/metronic/?page=docs&section=datatable
         datatable = $("#kt_apps_venue_list_datatable").KTDatatable({
             // datasource definition
             data: {
                 type: "remote",
                 source: {
                     read: {
-                        url:
-                            "https://keenthemes.com/metronic/themes/themes/metronic/dist/preview/inc/api/datatables/demos/default.php"
+                        url: "/venues",
+                        method: "GET",
+                        headers: {
+                            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
+                                "content"
+                            )
+                        }
                     }
                 },
                 pageSize: 10, // display 20 records per page
@@ -53,203 +57,32 @@ var KTAppVenueListDatatable = (function() {
                     textAlign: "center"
                 },
                 {
-                    field: "AgentName",
-                    title: "User",
+                    field: "VenueName",
+                    title: "Venue",
                     width: 200,
-                    // callback function support for column rendering
-                    template: function(data, i) {
-                        var number = 4 + i;
-                        while (number > 12) {
-                            number = number - 3;
-                        }
-                        var user_img = "100_" + number + ".jpg";
-
-                        var pos = KTUtil.getRandomInt(0, 5);
-                        var position = [
-                            "Developer",
-                            "Designer",
-                            "CEO",
-                            "Manager",
-                            "Architect",
-                            "Sales"
-                        ];
-
-                        var output = "";
-                        if (number > 5) {
-                            output =
-                                '<div class="kt-user-card-v2">\
-								<div class="kt-user-card-v2__pic">\
-									<img src="https://keenthemes.com/metronic/preview/assets/media/users/' +
-                                user_img +
-                                '" alt="photo">\
-								</div>\
-								<div class="kt-user-card-v2__details">\
-									<a href="#" class="kt-user-card-v2__name">' +
-                                data.CompanyAgent +
-                                '</a>\
-									<span class="kt-user-card-v2__desc">' +
-                                position[pos] +
-                                "</span>\
-								</div>\
-							</div>";
-                        } else {
-                            var stateNo = KTUtil.getRandomInt(0, 6);
-                            var states = [
-                                "success",
-                                "brand",
-                                "danger",
-                                "success",
-                                "warning",
-                                "primary",
-                                "info"
-                            ];
-                            var state = states[stateNo];
-
-                            output =
-                                '<div class="kt-user-card-v2">\
-								<div class="kt-user-card-v2__pic">\
-									<div class="kt-badge kt-badge--xl kt-badge--' +
-                                state +
-                                '">' +
-                                data.CompanyAgent.substring(0, 1) +
-                                '</div>\
-								</div>\
-								<div class="kt-user-card-v2__details">\
-									<a href="#" class="kt-user-card-v2__name">' +
-                                data.CompanyAgent +
-                                '</a>\
-									<span class="kt-user-card-v2__desc">' +
-                                position[pos] +
-                                "</span>\
-								</div>\
-							</div>";
-                        }
-
-                        return output;
-                    }
-                },
-                {
-                    field: "Country",
-                    title: "Country",
                     template: function(row) {
-                        return row.Country + " " + row.ShipCountry;
+                        return row.name;
                     }
                 },
                 {
-                    field: "ShipDate",
-                    title: "Ship Date",
-                    type: "date",
-                    format: "MM/DD/YYYY"
-                },
-                {
-                    field: "ShipName",
-                    title: "Company",
-                    width: "auto",
-                    autoHide: false,
-                    // callback function support for column rendering
-                    template: function(data, i) {
-                        var number = i + 1;
-                        while (number > 5) {
-                            number = number - 3;
-                        }
-                        var img = number + ".png";
-
-                        var skills = [
-                            "Angular, React",
-                            "Vue, Kendo",
-                            ".NET, Oracle, MySQL",
-                            "Node, SASS, Webpack",
-                            "MangoDB, Java",
-                            "HTML5, jQuery, CSS3"
-                        ];
-
-                        var output =
-                            '\
-                        <div class="kt-user-card-v2">\
-                            <div class="kt-user-card-v2__pic">\
-                                <img src="https://keenthemes.com/metronic/preview/assets/media/client-logos/logo' +
-                            img +
-                            '" alt="photo">\
-                            </div>\
-                            <div class="kt-user-card-v2__details">\
-                                <a href="#" class="kt-user-card-v2__name">' +
-                            data.CompanyName +
-                            '</a>\
-                                <span class="kt-user-card-v2__email">' +
-                            skills[number - 1] +
-                            "</span>\
-                            </div>\
-                        </div>";
-
-                        return output;
-                    }
-                },
-                {
-                    field: "Status",
-                    title: "Status",
-                    width: 100,
-                    // callback function support for column rendering
+                    field: "Address",
+                    title: "Address",
                     template: function(row) {
-                        var status = {
-                            1: {
-                                title: "Pending",
-                                class: " btn-label-brand"
-                            },
-                            2: {
-                                title: "Processing",
-                                class: " btn-label-danger"
-                            },
-                            3: {
-                                title: "Success",
-                                class: " btn-label-success"
-                            },
-                            4: {
-                                title: "Delivered",
-                                class: " btn-label-success"
-                            },
-                            5: {
-                                title: "Canceled",
-                                class: " btn-label-warning"
-                            },
-                            6: {
-                                title: "Done",
-                                class: " btn-label-danger"
-                            },
-                            7: {
-                                title: "On Hold",
-                                class: " btn-label-warning"
-                            }
-                        };
-                        return (
-                            '<span class="btn btn-bold btn-sm btn-font-sm ' +
-                            status[row.Status].class +
-                            '">' +
-                            status[row.Status].title +
-                            "</span>"
-                        );
+                        return row.address1 + " " + row.address2;
                     }
                 },
                 {
-                    width: 110,
-                    field: "Type",
-                    title: "Type",
-                    autoHide: false,
-                    // callback function support for column rendering
+                    field: "City",
+                    title: "City",
                     template: function(row) {
-                        var status = {
-                            1: { title: "Online", state: "danger" },
-                            2: { title: "Retail", state: "primary" },
-                            3: { title: "Direct", state: "success" }
-                        };
-                        return (
-                            '<span class="kt-badge kt-badge--' +
-                            status[row.Type].state +
-                            ' kt-badge--dot"></span>&nbsp;<span class="kt-font-bold kt-font-' +
-                            status[row.Type].state +
-                            '">' +
-                            status[row.Type].title +
-                            "</span>"
-                        );
+                        return row.city;
+                    }
+                },
+                {
+                    field: "State",
+                    title: "State",
+                    template: function(row) {
+                        return row.state;
                     }
                 },
                 {
@@ -283,12 +116,6 @@ var KTAppVenueListDatatable = (function() {
 											<a href="#" class="kt-nav__link">\
 												<i class="kt-nav__link-icon flaticon2-trash"></i>\
 												<span class="kt-nav__link-text">Delete</span>\
-											</a>\
-										</li>\
-										<li class="kt-nav__item">\
-											<a href="#" class="kt-nav__link">\
-												<i class="kt-nav__link-icon flaticon2-mail-1"></i>\
-												<span class="kt-nav__link-text">Export</span>\
 											</a>\
 										</li>\
 									</ul>\
@@ -387,76 +214,6 @@ var KTAppVenueListDatatable = (function() {
             });
     };
 
-    // selected records status update
-    var selectedStatusUpdate = function() {
-        $("#kt_subheader_group_actions_status_change").on(
-            "click",
-            "[data-toggle='status-change']",
-            function() {
-                var status = $(this)
-                    .find(".kt-nav__link-text")
-                    .html();
-
-                // fetch selected IDs
-                var ids = datatable
-                    .rows(".kt-datatable__row--active")
-                    .nodes()
-                    .find('.kt-checkbox--single > [type="checkbox"]')
-                    .map(function(i, chk) {
-                        return $(chk).val();
-                    });
-
-                if (ids.length > 0) {
-                    // learn more: https://sweetalert2.github.io/
-                    swal.fire({
-                        buttonsStyling: false,
-
-                        html:
-                            "Are you sure to update " +
-                            ids.length +
-                            " selected records status to " +
-                            status +
-                            " ?",
-                        type: "info",
-
-                        confirmButtonText: "Yes, update!",
-                        confirmButtonClass: "btn btn-sm btn-bold btn-brand",
-
-                        showCancelButton: true,
-                        cancelButtonText: "No, cancel",
-                        cancelButtonClass: "btn btn-sm btn-bold btn-default"
-                    }).then(function(result) {
-                        if (result.value) {
-                            swal.fire({
-                                title: "Deleted!",
-                                text:
-                                    "Your selected records statuses have been updated!",
-                                type: "success",
-                                buttonsStyling: false,
-                                confirmButtonText: "OK",
-                                confirmButtonClass:
-                                    "btn btn-sm btn-bold btn-brand"
-                            });
-                            // result.dismiss can be 'cancel', 'overlay',
-                            // 'close', and 'timer'
-                        } else if (result.dismiss === "cancel") {
-                            swal.fire({
-                                title: "Cancelled",
-                                text:
-                                    "You selected records statuses have not been updated!",
-                                type: "error",
-                                buttonsStyling: false,
-                                confirmButtonText: "OK",
-                                confirmButtonClass:
-                                    "btn btn-sm btn-bold btn-brand"
-                            });
-                        }
-                    });
-                }
-            }
-        );
-    };
-
     // selected records delete
     var selectedDelete = function() {
         $("#kt_subheader_group_actions_delete_all").on("click", function() {
@@ -527,7 +284,6 @@ var KTAppVenueListDatatable = (function() {
             search();
             selection();
             selectedFetch();
-            selectedStatusUpdate();
             selectedDelete();
             updateTotal();
         }
