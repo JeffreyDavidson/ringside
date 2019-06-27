0926f334f7802055b6113e7bba5f40afdf654b44
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-06-26 22:06:13 -0400

    Remove dead code

--- app/Models/Title.php
+++ app/Models/Title.php
@@ -2,18 +2,13 @@
 
 namespace App\Models;
 
-use App\Traits\Sluggable;
 use App\Traits\Retireable;
-use App\Traits\Activatable;
-use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Title extends Model
 {
-    use Sluggable,
-        Activatable,
-        Retireable,
+    use Retireable,
         SoftDeletes;
 
     /**
@@ -38,33 +33,4 @@ class Title extends Model
     protected $casts = [
         'is_active' => 'boolean',
     ];
-
-    /**
-     * Undocumented function
-     *
-     * @return void
-     */
-    public static function boot()
-    {
-        parent::boot();
-
-        static::creating(function ($model) {
-            $model->is_active = $model->introduced_at->lte(today());
-        });
-    }
-
-    /**
-     * Scope a query to only include titles of a given state.
-     *
-     * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function scopeHasState($query, $state)
-    {
-        $scope = 'scope' . Str::studly($state);
-
-        if (method_exists($this, $scope)) {
-            return $this->{$scope}($query);
-        }
-    }
 }
