5c59823c29650a49834a98cb1243edaf53925b32
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-07-03 19:43:05 -0400

    Restructure to single action controllers

--- app/Http/Controllers/Titles/UnretireController.php
+++ app/Http/Controllers/Titles/UnretireController.php
@@ -5,19 +5,19 @@ namespace App\Http\Controllers\Titles;
 use App\Models\Title;
 use App\Http\Controllers\Controller;
 
-class TitleActivationsController extends Controller
+class UnretireController extends Controller
 {
     /**
-     * Activate and inactive title.
+     * Unretire a title.
      *
      * @param  \App\Models\Title  $title
      * @return \lluminate\Http\RedirectResponse
      */
-    public function store(Title $title)
+    public function __invoke(Title $title)
     {
-        $this->authorize('activate', $title);
+        $this->authorize('unretire', $title);
 
-        $title->activate();
+        $title->unretire();
 
         return redirect()->route('titles.index');
     }
