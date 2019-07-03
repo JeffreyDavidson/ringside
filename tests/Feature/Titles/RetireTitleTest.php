28bcabe6a5a3b046ddd07fd633863d302b3cdf3e
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-07-03 08:45:58 -0400

    Fix failing test

--- tests/Feature/Titles/RetireTitleTest.php
+++ tests/Feature/Titles/RetireTitleTest.php
@@ -53,4 +53,15 @@ class RetireTitleTest extends TestCase
 
         $response->assertStatus(403);
     }
+
+    /** @test */
+    public function an_inactive_title_cannot_be_retired()
+    {
+        $this->actAs('administrator');
+        $title = factory(Title::class)->states('inactive')->create();
+
+        $response = $this->post(route('titles.retire', $title));
+
+        $response->assertStatus(403);
+    }
 }
