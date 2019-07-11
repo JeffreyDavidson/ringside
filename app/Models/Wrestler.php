6050c8e18053a9214d714ce47b83439cdef3bb9b
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-07-10 20:50:53 -0400

    Add class documentation

--- app/Models/Wrestler.php
+++ app/Models/Wrestler.php
@@ -7,6 +7,67 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * App\Models\Wrestler
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property string $name
+ * @property int $height
+ * @property int $weight
+ * @property string $hometown
+ * @property string|null $signature_move
+ * @property \Illuminate\Support\Carbon $hired_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read string $feet
+ * @property-read string $formatted_height
+ * @property-read string $formatted_hired_at
+ * @property-read string $inches
+ * @property-read bool $is_bookable
+ * @property-read bool $is_hired
+ * @property-read bool $is_injured
+ * @property-read bool $is_retired
+ * @property-read bool $is_suspended
+ * @property-read \App\Enum\WrestlerStatus $status
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Injury[] $injuries
+ * @property-read \App\Models\Injury $injury
+ * @property-read \App\Models\Retirement $retirement
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Retirement[] $retirements
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Stable[] $stable
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Stable[] $stables
+ * @property-read \App\Models\Suspension $suspension
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Suspension[] $suspensions
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\TagTeam[] $tagteam
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\TagTeam[] $tagteams
+ * @property-read \App\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler bookable()
+ * @method static bool|null forceDelete()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler inactive()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler injured()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler newQuery()
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Wrestler onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler query()
+ * @method static bool|null restore()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler retired()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler suspended()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler whereHeight($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler whereHiredAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler whereHometown($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler whereSignatureMove($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Wrestler whereWeight($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Wrestler withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Wrestler withoutTrashed()
+ * @mixin \Eloquent
+ */
 class Wrestler extends Model
 {
     use SoftDeletes;
