75fa3ec803545fc79e30b2c4d2d90a34952a192e
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-08-10 11:35:00 -0400

    Indent attributes to keep line length

--- resources/views/events/partials/filters.blade.php
+++ resources/views/events/partials/filters.blade.php
@@ -13,14 +13,25 @@
         <div class="form-group m-3">
             <label>Date:</label>
             <div class="input-group flatpickr kt-input-icon kt-input-icon--right">
-                <input class="form-control flatpickr-input" placeholder="Start Date" id="date_start" data-datetimepicker="" type="text" readonly="readonly">
+                <input class="form-control flatpickr-input"
+                    placeholder="Start Date"
+                    id="date_start"
+                    data-datetimepicker=""
+                    type="text"
+                    readonly="readonly"
+                >
                 <span class="kt-input-icon__icon kt-input-icon__icon--right">
                     <span><i class="flaticon-calendar-with-a-clock-time-tools"></i></span>
                 </span>
             </div>
             <small class="text-center font-weight-bold text-muted d-block">to</small>
             <div class="input-group flatpickr kt-input-icon kt-input-icon--right">
-                <input class="form-control flatpickr-input" placeholder="End Date" id="date_end" data-datetimepicker="" type="text" readonly="readonly">
+                <input class="form-control flatpickr-input"
+                    placeholder="End Date"
+                    id="date_end"
+                    data-datetimepicker=""
+                    type="text"
+                    readonly="readonly">
                 <span class="kt-input-icon__icon kt-input-icon__icon--right">
                     <span><i class="flaticon-calendar-with-a-clock-time-tools"></i></span>
                 </span>
