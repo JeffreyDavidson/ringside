c605a80a9d74bde339099db665143478e8bbbb0c
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-08-21 12:02:26 -0400

    Remove duplication to status trait

--- app/Filters/ManagerFilters.php
+++ app/Filters/ManagerFilters.php
@@ -2,31 +2,15 @@
 
 namespace App\Filters;
 
-use Illuminate\Support\Str;
-
 class ManagerFilters extends Filters
 {
     use Concerns\FiltersByStartDate,
+        Concerns\FiltersByStatus;
+
     /**
      * Registered filters to operate upon.
      *
      * @var array
      */
     protected $filters = ['status', 'started_at'];
-
-    /**
-     * Filter a query to include managers of a status.
-     *
-     * @param  string  $status
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function status($status)
-    {
-        if (method_exists($this->builder->getModel(), 'scope' . Str::studly($status))) {
-            $this->builder->{Str::camel($status)}();
-        }
-
-        return $this->builder;
-    }
-
 }
