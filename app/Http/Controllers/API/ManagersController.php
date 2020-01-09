57ea0ea0fc77fab183fa93d97274ffaa80e22bb2
Jefffrey Davidson <jeffrey.davidson@outlook.com>
2020-01-08 21:30:13 -0500

    Cleanup API Manager controller

--- app/Http/Controllers/API/ManagersController.php
+++ app/Http/Controllers/API/ManagersController.php
@@ -3,27 +3,21 @@
 namespace App\Http\Controllers\API;
 
 use App\Models\Manager;
-use App\Filters\ManagerFilters;
 use App\Http\Controllers\Controller;
 use App\DataTables\ManagersDataTable;
 
 class ManagersController extends Controller
 {
     /**
-     * Retrieve filtered list of managers.
+     * Retrieve list of managers.
      *
-     * @param  App\DataTables\ManagersDataTable  $table
-     * @param  App\Filters\ManagerFilters  $requestFilter
-     * @return \Illuminate\Response\
+     * @param  App\DataTables\ManagersDataTable  $dataTable
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index(ManagersDataTable $dataTable, ManagerFilters $requestFilter)
+    public function index(ManagersDataTable $dataTable)
     {
         $this->authorize('viewList', Manager::class);
 
-        $model = Manager::query();
-        // dd($dataTable->eloquent($model)->toJson());
-
-        // return $dataTable->eloquent($model)->toJson();
-        return $dataTable->of(Manager::query())->toJson();
+        return $dataTable->ajax();
     }
 }
