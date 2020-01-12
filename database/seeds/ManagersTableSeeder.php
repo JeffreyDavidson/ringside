a8d60e991edf66824a815a05c5b7676ecd6bd572
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2020-01-11 19:59:25 -0500

    Fix broken seeder model creation

--- database/seeds/ManagersTableSeeder.php
+++ database/seeds/ManagersTableSeeder.php
@@ -13,23 +13,24 @@ class ManagersTableSeeder extends Seeder
     public function run()
     {
         for ($w = 1; $w <= 20; $w++) {
-            factory(Manager::class)->create([
+            $manager = factory(Manager::class)->create([
                 'first_name' => 'Manager',
                 'last_name' => $w,
-            ])->employments()->create([
-                'started_at' => now()->subYears(1)
             ]);
+
+            $manager->employ(now()->subYears(1));
         }
 
         $eNum = 21;
         for ($i = 1; $i <= 15; $i++) {
             for ($j = 1; $j <= 2; $j++) {
-                factory(Manager::class)->create([
+                $manager = factory(Manager::class)->create([
                     'first_name' => 'Manager',
                     'last_name' => $eNum,
-                ])->employments()->create([
-                    'started_at' => now()->subYear(1)->addMonth($i)
                 ]);
+
+                $manager->employ(now()->subYear(1)->addMonth($i));
+
                 $eNum ++;
             }
         }
