46265a7e2cede2b20bfaf534743b2608a0747e65
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2020-01-14 20:19:01 -0500

    Add logic for started at property in view model

--- app/ViewModels/ManagerViewModel.php
+++ app/ViewModels/ManagerViewModel.php
@@ -7,8 +7,17 @@ use Spatie\ViewModels\ViewModel;
 
 class ManagerViewModel extends ViewModel
 {
+    /** @var $manager */
+    public $manager;
+
+    /**
+     * Undocumented function
+     *
+     * @param App\Models\Manager $manager
+     */
     public function __construct(Manager $manager = null)
     {
         $this->manager = $manager ?? new Manager;
+        $this->manager->started_at = optional($this->manager->started_at ?? null)->toDateTimeString();
     }
 }
