18c3932c2ad2d3143bdc9084f3bcedd508642ad2
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2021-09-02 21:06:05 -0400

    wip

--- tests/Feature/Http/Controllers/Events/EventControllerTest.php
+++ tests/Feature/Http/Controllers/Events/EventControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Http\Controllers\Events;
 use App\Enums\Role;
 use App\Http\Controllers\Events\EventsController;
 use App\Models\Event;
+use App\Models\Venue;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -70,7 +71,24 @@ class EventControllerTest extends TestCase
             ->actAs(Role::ADMINISTRATOR)
             ->get(action([EventsController::class, 'show'], $this->event))
             ->assertViewIs('events.show')
-            ->assertViewHas('event', $this->event);
+            ->assertViewHas('event', $this->event)
+            ->assertViewMissing('event.venue');
+    }
+
+    /**
+     * @test
+     */
+    public function show_returns_a_view_with_an_event_venue()
+    {
+        $venue = Venue::factory()->create();
+        $event = Event::factory()->create(['venue_id' => $venue->id]);
+
+        $this
+            ->actAs(Role::ADMINISTRATOR)
+            ->get(action([EventsController::class, 'show'], $event))
+            ->assertViewIs('events.show')
+            ->assertViewHas('event', $event)
+            ->assertViewHas('event.venue');
     }
 
     /**
