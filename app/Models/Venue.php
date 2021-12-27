375cfd5bc46eb67fbcffbed8ce04ca4edc2b0758
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2021-12-27 15:17:15 -0500

    wip

--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property int $id
+ */
 class Venue extends Model
 {
     use HasFactory,
