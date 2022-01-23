7de3fc8dbed19007d96465cc619adeb9e8858119
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-01-23 18:26:59 -0500

    wip

--- app/Actions/Stables/AddMembersAction.php
+++ app/Actions/Stables/AddMembersAction.php
@@ -3,6 +3,7 @@
 namespace App\Actions\Stables;
 
 use App\Models\Stable;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -16,18 +17,19 @@ class AddMembersAction extends BaseStableAction
      * @param  \App\Models\Stable  $stable
      * @param  \Illuminate\Database\Eloquent\Collection  $wrestlers
      * @param  \Illuminate\Database\Eloquent\Collection  $tagTeams
+     * @param  \Carbon\Carbon  $joinedDate
      *
      * @return void
      */
-    public function handle(Stable $stable, Collection $wrestlers, Collection $tagTeams): void
+    public function handle(Stable $stable, Collection $wrestlers, Collection $tagTeams, Carbon $joinedDate = null): void
     {
         $joinedDate ??= now();
 
-        if ($wrestlers) {
+        if ($wrestlers->isNotEmpty()) {
             $this->stableRepository->addWrestlers($stable, $wrestlers, $joinedDate);
         }
 
-        if ($tagTeams) {
+        if ($tagTeams->isNotEmpty()) {
             $this->stableRepository->addTagTeams($stable, $tagTeams, $joinedDate);
         }
     }
