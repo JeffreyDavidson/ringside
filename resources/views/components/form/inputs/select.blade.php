f88b0afeec984e4202f34283fcc43ee69c7917fb
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-02-03 20:31:20 -0500

    Update blade components

--- resources/views/components/form/inputs/select.blade.php
+++ resources/views/components/form/inputs/select.blade.php
@@ -9,6 +9,7 @@
     @foreach ($options as $key => $value)
         <option
             value="{{ $key }}"
+            @if ($selected === $key) selected @endif
         >{{ $value }}</option>
     @endforeach
 </select>
