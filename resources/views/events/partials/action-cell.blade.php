13c4fd58bb6808304b981b4ccce0ae41c5f68210
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-02-03 20:30:49 -0500

    Refactor views

--- resources/views/events/partials/action-cell.blade.php
+++ resources/views/events/partials/action-cell.blade.php
@@ -1,8 +1,4 @@
 <x-actions-dropdown>
-    @can('view', $event)
-        <x-buttons.view :route="route('events.show', $event)" />
-    @endcan
-
     @can('update', $event)
         <x-buttons.edit :route="route('events.edit', $event)" />
     @endcan
