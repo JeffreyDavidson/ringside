aea049b061ca1e65a593204c8244101dcf0c1a84
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-06-17 16:36:05 -0400

    Add test for action class

--- tests/Feature/Actions/Wrestlers/ClearInjuryActionTest.php
+++ tests/Feature/Actions/Wrestlers/ClearInjuryActionTest.php
@@ -2,11 +2,26 @@
 
 use App\Actions\Wrestlers\ClearInjuryAction;
 use App\Models\Wrestler;
+use Illuminate\Support\Carbon;
 
-test('invoke marks an injured wrestler as being cleared from injury and redirects', function () {
-    $wrestler = Wrestler::factory()->create();
+test('run makes injured wrestler not injured using current datetime', function () {
+    $wrestler = Wrestler::factory()->injured()->create();
+    $now = now();
 
     ClearInjuryAction::run($wrestler);
 
-    expect($wrestler->fresh())->isInjured()->toBeFalse();
+    expect($wrestler->fresh())
+        ->isInjured()->toBeFalse()
+        ->injuries->last()->ended_at->toEqual($now->toDateTimeString());
+});
+
+test('run makes injured wrestler not injured using specific datetime', function () {
+    $wrestler = Wrestler::factory()->injured()->create();
+    $recoveryDate = Carbon::parse('2022-05-27 12:00:00');
+
+    ClearInjuryAction::run($wrestler, $recoveryDate);
+
+    expect($wrestler->fresh())
+        ->isInjured()->toBeFalse()
+        ->injuries->last()->ended_at->toEqual($recoveryDate);
 });
