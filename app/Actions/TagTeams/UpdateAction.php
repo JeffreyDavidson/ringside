419e013cb9607b6b02c237c44157c82acf52d044
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-08-18 09:25:13 -0400

    Cleanup action classes

--- app/Actions/TagTeams/UpdateAction.php
+++ app/Actions/TagTeams/UpdateAction.php
@@ -6,6 +6,7 @@ namespace App\Actions\TagTeams;
 
 use App\Data\TagTeamData;
 use App\Models\TagTeam;
+use App\Models\Wrestler;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class UpdateAction extends BaseTagTeamAction
@@ -23,14 +24,20 @@ class UpdateAction extends BaseTagTeamAction
     {
         $this->tagTeamRepository->update($tagTeam, $tagTeamData);
 
-        if ($tagTeamData->wrestlers->isNotEmpty()) {
-            UpdateTagTeamPartnersAction::run($tagTeam, $tagTeamData->wrestlers);
+        $tagTeam->currentWrestlers
+            ->diff(Wrestler::whereIn('id', [$tagTeamData->wrestlerA->id, $tagTeamData->wrestlerB->id])->get())
+            ->each(fn ($wrestler) => RemoveTagTeamPartnerAction::run($tagTeam, $wrestler));
+
+        if ($tagTeamData->wrestlerA && $tagTeam->currentWrestlers->doesntContain('id', $tagTeamData->wrestlerA->id)) {
+            AddTagTeamPartnerAction::run($tagTeam, $tagTeamData->wrestlerA);
+        }
+
+        if ($tagTeamData->wrestlerB && $tagTeam->currentWrestlers->doesntContain('id', $tagTeamData->wrestlerB->id)) {
+            AddTagTeamPartnerAction::run($tagTeam, $tagTeamData->wrestlerB);
         }
 
         if (isset($tagTeamData->start_date)) {
-            if ($tagTeam->canBeEmployed()
-                || $tagTeam->canHaveEmploymentStartDateChanged($tagTeamData->start_date)
-            ) {
+            if ($tagTeam->canBeEmployed() || $tagTeam->canHaveEmploymentStartDateChanged($tagTeamData->start_date)) {
                 EmployAction::run($tagTeam, $tagTeamData->start_date);
             }
         }
