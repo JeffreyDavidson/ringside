8d35b0509c31c0207bf6d007a5f51b04ec5f8980
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-08-18 17:51:03 -0400

    Fix form layout for street address input

--- resources/views/venues/create.blade.php
+++ resources/views/venues/create.blade.php
@@ -25,24 +25,12 @@
                     />
                 </div>
                 <div class="mb-10">
-                    <div class="mb-5 row gx-10">
-                        <div class="col-lg-8">
-                            <x-form.inputs.text
-                                label="Street Address:"
-                                name="address1"
-                                placeholder="Street Address Here"
-                                :value="old('address1')"
-                            />
-                        </div>
-                        <div class="col-lg-4">
-                            <x-form.inputs.text
-                                label="Suite Number:"
-                                name="address2"
-                                placeholder="Suite Number Here"
-                                :value="old('address2')"
-                            />
-                        </div>
-                    </div>
+                    <x-form.inputs.text
+                        label="Street Address:"
+                        name="street_address"
+                        placeholder="Street Address Here"
+                        :value="old('street_address')"
+                    />
                 </div>
                 <div class="mb-10">
                     <div class="mb-5 row gx-10">
