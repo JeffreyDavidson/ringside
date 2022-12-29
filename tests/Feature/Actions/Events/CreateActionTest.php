5856703fe3779948233ae323e138cb49858510a0
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-12-29 09:00:29 -0500

    Fix event create action test

--- tests/Feature/Actions/Events/CreateActionTest.php
+++ tests/Feature/Actions/Events/CreateActionTest.php
@@ -5,12 +5,13 @@ use App\Data\EventData;
 use App\Repositories\EventRepository;
 
 test('it creates an event', function () {
-    $data = new EventData('Example Event Name', null, null, null, null, null);
+    $data = new EventData('Example Event Name', null, null, null);
 
-    CreateAction::run($data);
-
-    mock(EventRepository::class)
+    $this->mock(EventRepository::class)
         ->shouldReceive('create')
         ->once()
-        ->with($data);
+        ->with($data)
+        ->andReturns(new App\Models\Event());
+
+    CreateAction::run($data);
 });
