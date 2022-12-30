c75c4881ffe30f7acc554e5945db01810892ee83
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-12-29 19:16:17 -0500

    Added a needed validation rule

--- app/Http/Requests/Events/UpdateRequest.php
+++ app/Http/Requests/Events/UpdateRequest.php
@@ -49,7 +49,7 @@ class UpdateRequest extends FormRequest
         return [
             'name' => ['required', 'string', new LetterSpace, 'min:3', Rule::unique('events')->ignore($event)],
             'date' => ['nullable', 'string', 'date', new EventDateCanBeChanged($event)],
-            'venue_id' => ['nullable', 'integer', Rule::exists('venues', 'id')],
+            'venue_id' => ['nullable', 'required_with:date', 'integer', Rule::exists('venues', 'id')],
             'preview' => ['nullable', 'string'],
         ];
     }
