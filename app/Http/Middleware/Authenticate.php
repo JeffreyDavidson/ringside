1b4d13fa25822bed13ea34f80d0f93997328f7da
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2023-02-14 12:12:02 -0500

    Match method for L10 consistency

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,10 +12,8 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo(Request $request): string|null
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        return $request->expectsJson() ? null : route('login');
     }
 }
