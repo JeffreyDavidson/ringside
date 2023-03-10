4b4c617dbe6c9a7bcfb4701f7716b88486a65419
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2023-03-10 08:44:06 -0500

    Remove duplicate test

--- tests/Unit/Models/WrestlerTest.php
+++ tests/Unit/Models/WrestlerTest.php
@@ -203,28 +203,3 @@ test('a wrestler belongs to many tag teams', function () {
     assertCount(1, $wrestler->previousTagTeams);
     assertTrue($wrestler->previousTagTeam->is($tagTeamA));
 });
-
-test('a wrestler belongs to many tag teams', function () {
-    $wrestler = Wrestler::factory()->create();
-    $tagTeamA = TagTeam::factory()->bookable()->create(['name' => 'Tag Team A']);
-    $tagTeamB = TagTeam::factory()->bookable()->create(['name' => 'Tag Team B']);
-
-    $wrestler->tagTeams()->attach($tagTeamA, ['joined_at' => now()]);
-
-    $wrestler->refresh();
-
-    assertCount(1, $wrestler->tagTeams);
-    assertCount(0, $wrestler->previousTagTeams);
-    assertTrue($wrestler->currentTagTeam->is($tagTeamA));
-
-    $wrestler->tagTeams()->updateExistingPivot($tagTeamA->id, ['left_at' => now()]);
-
-    $wrestler->tagTeams()->attach($tagTeamB, ['joined_at' => now()]);
-
-    $wrestler->refresh();
-
-    assertCount(2, $wrestler->tagTeams);
-    assertTrue($wrestler->currentTagTeam->is($tagTeamB));
-    assertCount(1, $wrestler->previousTagTeams);
-    assertTrue($wrestler->previousTagTeam->is($tagTeamA));
-});
