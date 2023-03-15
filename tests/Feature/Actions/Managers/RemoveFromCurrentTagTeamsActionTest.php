fe7b5707019841bcba93a28674ddfa4faf3251fa
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2023-03-15 07:46:28 -0400

    wip

--- tests/Feature/Actions/Managers/RemoveFromCurrentTagTeamsActionTest.php
+++ tests/Feature/Actions/Managers/RemoveFromCurrentTagTeamsActionTest.php
@@ -4,11 +4,18 @@ use App\Actions\Managers\RemoveFromCurrentTagTeamsAction;
 use App\Models\Manager;
 use App\Repositories\ManagerRepository;
 use function Pest\Laravel\mock;
+use Illuminate\Support\Facades\Event;
+
+beforeEach(function () {
+    Event::fake();
+
+    $this->managerRepository = mock(ManagerRepository::class);
+});
 
 test('it can remove current tag teams from a manager', function () {
     $manager = Manager::factory()->create();
 
-    mock(ManagerRepository::class)
+    $this->managerRepository
         ->shouldReceive('removeFromCurrentTagTeams')
         ->once()
         ->with($manager);
