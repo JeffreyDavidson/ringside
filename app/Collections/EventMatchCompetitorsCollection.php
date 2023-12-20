339b025d8e77a10efd764a1ea4710bb0ebd2b214
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2023-12-20 10:50:52 -0500

    Fix custom collection

--- app/Collections/EventMatchCompetitorsCollection.php
+++ app/Collections/EventMatchCompetitorsCollection.php
@@ -7,10 +7,18 @@ namespace App\Collections;
 use App\Models\EventMatchCompetitor;
 use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * @template TKey of array-key
+ * @template TModel of \Illuminate\Database\Eloquent\Model
+ *
+ * @extends \Illuminate\Database\Eloquent\Collection<TKey, TModel>
+ */
 class EventMatchCompetitorsCollection extends Collection
 {
     /**
      * Get all competitors for a match grouped by side.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection<array-key, \Illuminate\Database\Eloquent\Collection<(int|string), \App\Models\EventMatchCompetitor>>
      */
     public function groupedBySide(): Collection
     {
