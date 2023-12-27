17c44ca8f07da5ad6524a3bd32481aefdaf7e4d3
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2023-12-27 14:03:45 -0500

    Fix views for a tag team

---
+++ database/migrations/2023_12_26_000029_create_event_match_results.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\MatchDecision;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_match_results', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_match_id')->constrained();
+            $table->morphs('winner');
+            $table->foreignIdFor(MatchDecision::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_match_results');
+    }
+};
