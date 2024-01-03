06c79466aeacdedaf94ce3657e35e8c164b0e0d9
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2024-01-03 04:01:47 -0500

    Convert to use knew child components

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -12,24 +12,28 @@
         </x-toolbar>
     </x-slot>
 
-    <x-details-card>
-        <x-card>
-            <x-card.body>
-                <x-card.detail-link collapsibleLink="kt_event_view_details" resource="event" :href="route('events.edit', $event)" />
-                <x-separator />
-                <x-card.detail-container id="kt_event_view_details">
-                    <x-card.detail-row property="Name" value="{{ $event->name }}" />
-                    <x-card.detail-row property="Date" value="{{ $event->date?->format('Y-m-j g:i A') ?? 'Unscheduled' }}" />
-                    <x-card.detail-row property="Venue" value="{{ $event->venue->name ?? 'No Venue Chosen' }}" />
-                    <x-card.detail-row property="Preview" value="{{ $event->preview ?? 'No preview added'}}" />
-                </x-card.detail-container>
+    <x-details-page>
+        <x-details-card>
+            <x-card>
+                <x-card.body>
+                    <x-card.detail-link collapsibleLink="kt_event_view_details" resource="event" :href="route('events.edit', $event)" />
+                    <x-separator />
+                    <x-card.detail-container id="kt_event_view_details">
+                        <x-card.detail-row property="Name" value="{{ $event->name }}" />
+                        <x-card.detail-row property="Date" value="{{ $event->date?->format('Y-m-j g:i A') ?? 'Unscheduled' }}" />
+                        <x-card.detail-row property="Venue" value="{{ $event->venue->name ?? 'No Venue Chosen' }}" />
+                        <x-card.detail-row property="Preview" value="{{ $event->preview ?? 'No preview added'}}" />
+                    </x-card.detail-container>
 
-                @if ($event->isUnscheduled())
-                    <x-notice class="mt-4" title="This event needs your attention!" description="This event does not have a date and needs to be scheduled." />
-                @endif
-            </x-card.body>
-        </x-card>
-    </x-details-card>
+                    @if ($event->isUnscheduled())
+                        <x-notice class="mt-4" title="This event needs your attention!" description="This event does not have a date and needs to be scheduled." />
+                    @endif
+                </x-card.body>
+            </x-card>
+        </x-details-card>
 
-    @include('events.partials.matches', ['matches' => $event->matches])
+        <x-details-data>
+            @include('events.partials.matches', ['matches' => $event->matches])
+        </x-details-data>
+    </x-details-page>
 </x-layouts.app>
