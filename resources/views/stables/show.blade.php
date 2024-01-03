dbe2bae504130226574aad241057bbd0e6fe7a17
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2024-01-03 02:35:48 -0500

    Change fields to use new child components

--- resources/views/stables/show.blade.php
+++ resources/views/stables/show.blade.php
@@ -12,20 +12,28 @@
         </x-toolbar>
     </x-slot>
 
-    <x-details-card>
-        <x-card>
-            <x-card.body>
-                <x-card.detail-link collapsibleLink="kt_stable_view_details" resource="stable" :href="route('stables.edit', $stable)" />
-                <x-separator />
-                <x-card.detail-container id="kt_stable_view_details">
-                    <x-card.detail-row property="Name" value="{{ $stable->name }}" />
-                    <x-card.detail-row property="Start Date" value="{{ $stable->activatedAt?->toDateString() ?? 'No Activation Date Set' }}" />
-                </x-card.detail-container>
+    <x-details-page>
+        <x-details-card>
+            <x-card>
+                <x-card.body>
+                    <x-card.detail-link collapsibleLink="kt_stable_view_details" resource="stable" :href="route('stables.edit', $stable)" />
+                    <x-separator />
+                    <x-card.detail-container id="kt_stable_view_details">
+                        <x-card.detail-row>
+                            <x-card.detail-property label="Name" />
+                            <x-card.detail-value>{{ $stable->name }}</x-card.detail-value>
+                        </x-card.detail-row>
+                        <x-card.detail-row>
+                            <x-card.detail-property label="Start Date" />
+                            <x-card.detail-value>{{ $stable->activatedAt?->toDateString() ?? 'No Activation Date Set' }}</x-card.detail-value>
+                        </x-card.detail-row>
+                    </x-card.detail-container>
 
-                @if ($stable->isUnactivated())
-                    <x-notice class="mt-4" title="This stable needs your attention!" description="This stable does not have a start date and needs to be started." />
-                @endif
-            </x-card.body>
-        </x-card>
-    </x-details-card>
+                    @if ($stable->isUnactivated())
+                        <x-notice class="mt-4" title="This stable needs your attention!" description="This stable does not have a start date and needs to be started." />
+                    @endif
+                </x-card.body>
+            </x-card>
+        </x-details-card>
+    </x-details-page>
 </x-layouts.app>
