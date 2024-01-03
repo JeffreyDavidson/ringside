096b36de8472cc008170a2f5cc551afe9be96034
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2024-01-03 02:36:47 -0500

    Fix line length

--- resources/views/stables/show.blade.php
+++ resources/views/stables/show.blade.php
@@ -16,7 +16,11 @@
         <x-details-card>
             <x-card>
                 <x-card.body>
-                    <x-card.detail-link collapsibleLink="kt_stable_view_details" resource="stable" :href="route('stables.edit', $stable)" />
+                    <x-card.detail-link
+                        collapsibleLink="kt_stable_view_details"
+                        resource="stable"
+                        :href="route('stables.edit', $stable)"
+                    />
                     <x-separator />
                     <x-card.detail-container id="kt_stable_view_details">
                         <x-card.detail-row>
@@ -25,12 +29,18 @@
                         </x-card.detail-row>
                         <x-card.detail-row>
                             <x-card.detail-property label="Start Date" />
-                            <x-card.detail-value>{{ $stable->activatedAt?->toDateString() ?? 'No Activation Date Set' }}</x-card.detail-value>
+                            <x-card.detail-value>
+                                {{ $stable->activatedAt?->toDateString() ?? 'No Activation Date Set' }}
+                            </x-card.detail-value>
                         </x-card.detail-row>
                     </x-card.detail-container>
 
                     @if ($stable->isUnactivated())
-                        <x-notice class="mt-4" title="This stable needs your attention!" description="This stable does not have a start date and needs to be started." />
+                        <x-notice
+                            class="mt-4"
+                            title="This stable needs your attention!"
+                            description="This stable does not have a start date and needs to be started."
+                        />
                     @endif
                 </x-card.body>
             </x-card>
