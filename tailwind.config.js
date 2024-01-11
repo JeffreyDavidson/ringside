e1165bf13bf0bcf3dce51915bf6786809fb3fbf4
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2024-01-11 13:20:28 -0500

    Remove forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import forms from '@tailwindcss/forms';
-
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -10,10 +8,4 @@ export default {
     theme: {
         extend: {},
     },
-
-    variants: {
-        extend: {},
-    },
-
-    plugins: [forms],
 };
