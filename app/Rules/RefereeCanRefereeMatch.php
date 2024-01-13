08b273859722695741fefc14563afd6d89c7522b
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2024-01-12 21:31:59 -0500

    Move phpcs suppress nnotation to beginning of docblock annotations

--- app/Rules/RefereeCanRefereeMatch.php
+++ app/Rules/RefereeCanRefereeMatch.php
@@ -13,9 +13,9 @@ class RefereeCanRefereeMatch implements ValidationRule
     /**
      * Run the validation rule.
      *
-     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
-     *
      * @phpcsSuppress SlevomatCodingStandard.Functions.UnusedParameter
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
