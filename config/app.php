8722a7e30f6e3008b88882badc8eebed1bb28b48
Shift <shift@laravelshift.com>
2024-03-14 22:21:23 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -7,21 +7,6 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\ValidationServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Redis' => Illuminate\Support\Facades\Redis::class,
