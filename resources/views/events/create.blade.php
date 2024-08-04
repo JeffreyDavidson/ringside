49166ffdf449e2c9400f7f3aece1c943d70634fe
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-08-03 20:26:19 -0400

    Refactor views to a single form view

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -12,49 +12,20 @@
         </x-toolbar>
     </x-slot>
 
-    <div class="shadow-sm card">
-        <div class="card-header">
-            <h3 class="card-title">Create A New Event Form</h3>
-        </div>
-        <div class="card-body">
-            <form method="post" action="{{ route('events.store') }}">
-                @csrf
-                <div class="mb-10">
-                    <x-form.inputs.text
-                        label="Name:"
-                        name="name"
-                        placeholder="Event Name Here"
-                        :value="old('name')"
-                    />
-                </div>
-                <div class="mb-10">
-                    <x-form.inputs.date
-                        label="Date:"
-                        name="date"
-                        :value="old('date')"
-                    />
-                </div>
-                <div class="mb-10">
-                    <x-form.inputs.select
-                        label="Venue:"
-                        name="venue_id"
-                        :options="$venues"
-                        :selected="old('venue_id')"
-                    />
-                </div>
-                <div class="mb-10">
-                    <x-form.inputs.textarea
-                        label="Preview:"
-                        name="preview"
-                        placeholder="Enter a preview description of the event."
-                        :value="old('preview')"
-                    />
-                </div>
-        </div>
-        <div class="card-footer">
-            <x-form.buttons.submit />
-            <x-form.buttons.reset />
-        </div>
-        </form>
-    </div>
+    <x-card>
+        <x-slot name="header">
+            <x-card.header title="Create A New Event Form" />
+        </x-slot>
+        <x-card.body>
+            <x-form :action="route('events.store')" id="createEventForm">
+                @include('events.partials.form')
+            </x-form>
+        </x-card.body>
+        <x-slot name="footer">
+            <x-card.footer>
+                <x-form.buttons.reset form="createEventForm"/>
+                <x-form.buttons.submit form="createEventForm"/>
+            </x-card.footer>
+        </x-slot>
+    </x-card>
 </x-layouts.app>
