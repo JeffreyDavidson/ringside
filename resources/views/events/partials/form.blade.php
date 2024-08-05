7689b1180825b04078774ac200369c483150ed55
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-08-05 14:38:25 -0400

    Clean up form inputs

--- resources/views/events/partials/form.blade.php
+++ resources/views/events/partials/form.blade.php
@@ -1,31 +1,15 @@
 <div class="mb-10">
-    <x-form.inputs.text
-        label="Name:"
-        name="name"
-        placeholder="Event Name Here"
-        :value="old('name')"
-    />
+    <x-form.inputs.text label="Name:" name="name" placeholder="Event Name Here" :value="old('name')"/>
 </div>
+
 <div class="mb-10">
-    <x-form.inputs.date
-        label="Date:"
-        name="date"
-        :value="old('date')"
-    />
+    <x-form.inputs.date label="Date:" name="date" :value="old('date')"/>
 </div>
+
 <div class="mb-10">
-    <x-form.inputs.select
-        label="Venue:"
-        name="venue_id"
-        :options="$venues"
-        :selected="old('venue_id')"
-    />
+    <x-form.inputs.select label="Venue:" name="venue_id" :options="$venues" :selected="old('venue_id')"/>
 </div>
+
 <div class="mb-10">
-    <x-form.inputs.textarea
-        label="Preview:"
-        name="preview"
-        placeholder="Enter a preview description of the event."
-        :value="old('preview')"
-    />
+    <x-form.inputs.textarea label="Preview:" name="preview" placeholder="Enter a preview description of the event." :value="old('preview')"/>
 </div>
