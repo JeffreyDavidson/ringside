1ffe034a0a7782cd64aecbdb38d2c4d4dd9212b7
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-09-24 17:31:03 -0400

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,8 @@ import './bootstrap';
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import Clipboard from '@ryangjchandler/alpine-clipboard'
 import '../../vendor/rappasoft/laravel-livewire-tables/resources/imports/laravel-livewire-tables-all.js';
-import '../css/app.css'; 
+import '../vendors/keenicons/styles.bundle.css';
+import '../css/app.css';
 
 Alpine.plugin(Clipboard)
 Livewire.start();
-
