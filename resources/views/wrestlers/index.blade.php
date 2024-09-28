6cb4dd2df22c3e51425f8dbdc9ad92a3f989ca27
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-09-28 18:52:30 -0400

    Add markup for adding a wrestler

--- resources/views/wrestlers/index.blade.php
+++ resources/views/wrestlers/index.blade.php
@@ -1,5 +1,22 @@
 <x-layouts.app>
     <x-container-fixed>
+        <div class="flex flex-wrap items-center lg:items-end justify-between gap-5 pb-7.5">
+            <div class="flex flex-col justify-center gap-2">
+                <h1 class="text-xl font-medium leading-none text-gray-900">
+                    Wrestlers
+                </h1>
+            </div>
+            <div class="flex items-center gap-2.5">
+                <div class="flex justify-items-end gap-2.5">
+                    <button class="btn btn-sm btn-primary"
+                        wire:click="$dispatch('openModal', { component: 'wrestlers.wrestler-modal' })">
+                        Add Wrestler
+                    </button>
+                </div>
+            </div>
+        </div>
+    </x-container-fixed>
+    <x-container-fixed>
         <livewire:wrestlers.wrestlers-table />
     </x-container-fixed>
 </x-layouts.app>
