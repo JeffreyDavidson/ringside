7a9dcae5d539963d0db2b907bf71afef51481f09
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-09-28 19:13:37 -0400

    WIP Changes to WrestlersTable

--- app/Livewire/Wrestlers/WrestlersTable.php
+++ app/Livewire/Wrestlers/WrestlersTable.php
@@ -9,7 +9,6 @@ use App\Livewire\Concerns\BaseTableTrait;
 use App\Models\Wrestler;
 use Illuminate\Support\Facades\Gate;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
-use Rappasoft\LaravelLivewireTables\Views\Actions\Action;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 
 class WrestlersTable extends DataTableComponent
@@ -22,14 +21,34 @@ class WrestlersTable extends DataTableComponent
 
     protected array $actionLinksToDisplay = ['view' => true, 'edit' => true, 'delete' => true];
 
-    public function configure(): void {}
+    public function configure(): void
+    {
+        $this->setThAttributes(function (Column $column) {
+            if ($column->getTitle() === __('core.actions')) {
+                return [
+                    'class' => 'w-[60px]',
+                ];
+            }
+
+            return [];
+        });
+
+        $this->setTdAttributes(function (Column $column) {
+            if ($column->getTitle() === __('core.actions')) {
+                return [
+                    'class' => 'text-center',
+                    'default' => false,
+                ];
+            }
+
+            return [];
+        });
+    }
 
     public function builder(): WrestlerBuilder
     {
-        return Wrestler::query()
-            ->with('employments:id,started_at')->withWhereHas('employments', function ($query) {
-                $query->where('started_at', '<=', now())->whereNull('ended_at')->limit(1);
-            });
+        return Wrestler::query();
+        // ->with('employments:id,started_at');
     }
 
     public function delete(Wrestler $model)
@@ -53,9 +72,9 @@ class WrestlersTable extends DataTableComponent
             Column::make(__('wrestlers.height'), 'height'),
             Column::make(__('wrestlers.weight'), 'weight'),
             Column::make(__('wrestlers.hometown'), 'hometown'),
-            Column::make(__('employments.start_date'), 'started_at')
-                ->label(fn ($row, Column $column) => $row->employments->first()->started_at->format('Y-m-d')),
-            Column::make(__('core.actions'), 'actions')
+            // Column::make(__('employments.start_date'), 'started_at')
+            //     ->label(fn ($row, Column $column) => $row->employments->first()->started_at->format('Y-m-d')),
+            Column::make(__('core.actions'))
                 ->label(
                     fn ($row, Column $column) => view('tables.columns.action-column')->with(
                         [
@@ -65,15 +84,6 @@ class WrestlersTable extends DataTableComponent
                         ]
                     )
                 )->html(),
-        ];
-    }
-
-    public function actions(): array
-    {
-        return [
-            Action::make('Create')
-                ->setWireAction('wire:click')
-                ->setWireActionDispatchParams("'openModal', { component: 'wrestlers.wrestler-modal' }"),
         ];
     }
 }
