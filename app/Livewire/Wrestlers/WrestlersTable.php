9290828adfa49a2d454a0fb2dddac8d39aa32e60
JeffreyDavidson <JeffreyDavidson@users.noreply.github.com>
2024-09-29 00:30:52 +0000

    Fix styling

--- app/Livewire/Wrestlers/WrestlersTable.php
+++ app/Livewire/Wrestlers/WrestlersTable.php
@@ -93,7 +93,7 @@ class WrestlersTable extends DataTableComponent
     public function actions(): array
     {
         return [
-            Action::make('Create')->setWireAction("wire:click")->setWireActionDispatchParams("'openModal', { component: '".$this->modalPath."' }")
+            Action::make('Create')->setWireAction('wire:click')->setWireActionDispatchParams("'openModal', { component: '".$this->modalPath."' }"),
         ];
     }
 }
