bf2172148582e0831b2c8df228868021b037ed5f
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-01 20:32:37 -0400

    Install a couple of alpine plugins

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,12 @@ import './bootstrap';
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import Clipboard from '@ryangjchandler/alpine-clipboard'
 import '../../vendor/rappasoft/laravel-livewire-tables/resources/imports/laravel-livewire-tables-all.js';
+import focus from '@alpinejs/focus';
+import ui from '@alpinejs/ui';
 import '../vendors/keenicons/styles.bundle.css';
 import '../css/app.css';
 
 Alpine.plugin(Clipboard)
+Alpine.plugin(focus)
+Alpine.plugin(ui)
 Livewire.start();
