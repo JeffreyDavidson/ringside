014ba220cff3cb86ce196bd0a07243caffa2a995
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-03 16:30:20 -0400

    Modify current actions dropdown

--- resources/views/tables/columns/action-column.blade.php
+++ resources/views/tables/columns/action-column.blade.php
@@ -1,11 +1,42 @@
-<div class="p-4 inline-flex " x-data="{ open: false, toggle() { this.open = !this.open }  }" @click.outside="open = false">
+<div class="p-4 inline-flex " x-data="{ open: false, toggle() { this.open = !this.open } }" @click.outside="open = false">
     <button x-ref="button" @click="toggle()" class="w-4 text-white rounded-md bg-black">&vellip;</button>
-    <div x-show.important="open" x-anchor.bottom="$refs.button" class="text-white bg-black rounded-md w-full max-w-[175px]">
-        <ul class="w-full ">
-            <li class="hover:bg-gray-500 hover:text-black px-4" @click="open = false">Item 1</li>
-            <li class="hover:bg-gray-500 hover:text-black px-4" @click="open = false">Item 2</li>
-            <li class="hover:bg-gray-500 hover:text-black px-4" @click="open = false">Item 3</li>
-            <li class="hover:bg-gray-500 hover:text-black px-4" @click="open = false">Item 4</li>
+    <div x-show.important="open" x-anchor.bottom="$refs.button"
+        class="menu-default text-white bg-black rounded-md w-full max-w-[175px]">
+        <ul class="w-full">
+            @if ($links['view'] ?? true)
+                <li class="menu-item" @click="open = false">
+                    <a class="menu-link" href="{{ route($path . '.show', $rowId) }}">
+                        <span class="menu-icon">
+                            <i class="ki-filled ki-search-list"></i>
+                        </span>
+                        <span class="menu-title">View</span>
+                    </a>
+                </li>
+            @endif
+            <div class="menu-separator"></div>
+            @if ($links['edit'] ?? true)
+                <li class="menu-item" @click="open = false">
+                    <button class="menu-link"
+                        wire:click="$dispatch('openModal', { component: 'wrestlers.wrestler-modal', arguments: { wrestler: {{ $rowId }} }})">
+                        <span class="menu-icon">
+                            <i class="ki-filled ki-pencil"></i>
+                        </span>
+                        <span class="menu-title">Edit</span>
+                    </button>
+                </li>
+            @endif
+            <div class="menu-separator"></div>
+            @if ($links['delete'] ?? true)
+                <li class="menu-item" @click="open = false">
+                    <button class="menu-link"
+                        wire:click="$dispatch('openModal', { component: 'wrestlers.delete-wrestler', arguments: { wrestler: {{ $rowId }} }})">
+                        <span class="menu-icon">
+                            <i class="ki-filled ki-trash"></i>
+                        </span>
+                        <span class="menu-title">Remove</span>
+                    </button>
+                </li>
+            @endif
         </ul>
     </div>
 </div>
