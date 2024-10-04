602605b52027c30d0aa9a55228459abaf03cb0af
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-03 21:01:15 -0400

    Add missing lang entries

--- lang/en/wrestlers.php
+++ lang/en/wrestlers.php
@@ -4,6 +4,9 @@ return [
     'name' => 'Name',
     'status' => 'Status',
     'height' => 'Height',
+    'feet' => 'Feet',
+    'inches' => 'Inches',
     'weight' => 'Weight',
     'hometown' => 'Hometown',
+    'signature_move' => 'Signature Move',
 ];
