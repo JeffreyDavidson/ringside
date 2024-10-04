5492f4d0cb4640e4cd112bcab2d5bd47d50db135
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-03 21:02:57 -0400

    Remove modal css from being bundled

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,6 @@ import Clipboard from '@ryangjchandler/alpine-clipboard'
 import '../../vendor/rappasoft/laravel-livewire-tables/resources/imports/laravel-livewire-tables-all.js';
 import ui from '@alpinejs/ui';
 import '../vendors/keenicons/styles.bundle.css';
-import '../css/modal.css';
 import '../css/app.css';
 
 Alpine.plugin(Clipboard)
