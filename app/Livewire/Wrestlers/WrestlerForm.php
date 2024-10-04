0d8bdd131b1765ffe81d934d1be5051022a3d8e1
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-03 21:00:58 -0400

    Add validation rules for form inputs

--- app/Livewire/Wrestlers/WrestlerForm.php
+++ app/Livewire/Wrestlers/WrestlerForm.php
@@ -4,26 +4,45 @@ declare(strict_types=1);
 
 namespace App\Livewire\Wrestlers;
 
-use Livewire\Attributes\Validate;
 use Livewire\Form;
+use Illuminate\Validation\Rule;
+use Livewire\Attributes\Validate;
 
 class WrestlerForm extends Form
 {
-    #[Validate('required|min:5')]
+    #[Validate(as: 'wrestlers.name')]
     public $name = '';
 
+    #[Validate(as: 'wrestlers.feet')]
     public int $height_feet;
 
+    #[Validate(as: 'wrestlers.inches')]
     public int $height_inches;
 
-    #[Validate('required|min:1')]
+    #[Validate(as: 'wrestlers.weight')]
     public $weight = '';
 
-    #[Validate('required|min:1')]
+    #[Validate(as: 'wrestlers.hometown')]
     public $hometown = '';
 
-    #[Validate('required|min:1')]
+    #[Validate(as: 'wrestlers.signature_move')]
     public $signature_move = '';
+
+    #[Validate(as: 'employments.start_date')]
+    public $start_date = '';
+
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'min:3', Rule::unique('wrestlers')],
+            'height_feet' => ['required', 'integer', 'max:8'],
+            'height_inches' => ['required', 'integer', 'max:11'],
+            'weight' => ['required', 'integer', 'digits:3'],
+            'hometown' => ['required', 'string'],
+            'signature_move' => ['nullable', 'string', 'regex:/^[a-zA-Z\s\']+$/'],
+            'start_date' => ['nullable', 'string', 'date'],
+        ];
+    }
 
     public function getHeight()
     {
