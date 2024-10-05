9f357bd1810eabe2062d1e489af1d17a67e8a5a3
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-05 10:48:00 -0400

    Refactor to a modal component

---
+++ resources/views/components/modal/index.blade.php
@@ -0,0 +1,5 @@
+<div class="w-full overflow-auto">
+    <div class="flex flex-col relative mx-auto rounded-xl max-w-[600] top-[10%] shadow-[--tw-modal-box-shadow] dark:shadow[--tw-modal-box-shadow] bg-[--tw-modal-background-color] dark:bg-[--tw-modal-background-color]">
+        {{ $slot }}
+    </div>
+</div>
--- resources/views/livewire/wrestlers/modals/form-modal.blade.php
+++ resources/views/livewire/wrestlers/modals/form-modal.blade.php
@@ -1,18 +1,16 @@
-<div class="w-full overflow-auto">
-    <div class="flex flex-col relative mx-auto rounded-xl max-w-[600] top-[10%] shadow-[--tw-modal-box-shadow] dark:shadow[--tw-modal-box-shadow] bg-[--tw-modal-background-color] dark:bg-[--tw-modal-background-color]">
-        <x-modal.header>
-            <x-modal.heading-title>{{ $this->getModalTitle() }}</x-modal.heading-title>
-        </x-modal.header>
+<x-modal>
+    <x-modal.header>
+        <x-modal.heading-title>{{ $this->getModalTitle() }}</x-modal.heading-title>
+    </x-modal.header>
 
-        <x-modal.content>
-            <x-wrestlers.form id="wrestlerForm"/>
-        </x-modal.content>
+    <x-modal.content>
+        <x-wrestlers.form id="wrestlerForm"/>
+    </x-modal.content>
 
-        <x-modal.footer>
-            <div class="flex gap-4">
-                <x-buttons.secondary wire:click="closeModal">Cancel</x-buttons.secondary>
-                <x-buttons.primary type="submit" form="wrestlerForm">Submit</x-buttons.primary>
-            </div>
-        </x-modal.footer>
-    </div>
-</div>
+    <x-modal.footer>
+        <div class="flex gap-4">
+            <x-buttons.secondary wire:click="closeModal">Cancel</x-buttons.secondary>
+            <x-buttons.primary type="submit" form="wrestlerForm">Submit</x-buttons.primary>
+        </div>
+    </x-modal.footer>
+</x-modal>
