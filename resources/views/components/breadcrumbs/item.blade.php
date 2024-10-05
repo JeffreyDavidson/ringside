ad95583e2a9cb018730b436da58419936588b9dd
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-05 11:26:53 -0400

    Delete unused components

--- resources/views/components/breadcrumbs/item.blade.php
+++
@@ -1,7 +0,0 @@
-<li class="breadcrumb-item text-muted">
-    @isset ($url)
-        <a href="{{ $url }}" class="text-muted text-hover-primary">{{ $label }}</a>
-    @else
-        {{ $label }}
-    @endisset
-</li>
--- resources/views/components/breadcrumbs/separator.blade.php
+++
@@ -1,3 +0,0 @@
-<li class="breadcrumb-item">
-    <span class="bg-gray-400 bullet w-5px h-2px"></span>
-</li>
