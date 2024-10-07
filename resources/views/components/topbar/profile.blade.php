9960445b26180bdf3eeaf26b2de16f7c99489ea5
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-07 18:09:09 -0400

    Push top bar changes

--- resources/views/components/topbar/profile.blade.php
+++ resources/views/components/topbar/profile.blade.php
@@ -1,12 +1,13 @@
-<div class="menu" data-menu="true">
-    <div class="menu-item" data-menu-item-offset="20px, 10px" data-menu-item-placement="bottom-end"
-        data-menu-item-toggle="dropdown" data-menu-item-trigger="click|lg:click">
-        <div class="menu-toggle btn btn-icon rounded-full">
+<div class="flex" x-data="{ open: false, toggle() { this.open = !this.open } }" x-on:click.outside="open = false">
+    <div class="flex flex-col">
+        <button x-ref="button" x-on:click="toggle()"
+            class="inline-flex align-center cursor-pointer leading-4 h-10 font-medium border-1 border-solid border-transparent justify-center shrink-0 p-0 gap-0 w-10 rounded-full grow">
             <img alt="" class="size-9 rounded-full border-2 border-success shrink-0"
                 src="{{ asset('assets/media/avatars/300-2.png') }}">
             </img>
-        </div>
-        <div class="menu-dropdown menu-default light:border-gray-300 w-screen max-w-[250px]">
+        </button>
+        <div x-show="open"
+            class="fixed z-[105px] m-0 light:border-gray-300 w-screen max-w-[250px] bottom-auto left-auto top-0 right-0 [transform:translate3d(-350px,65px,0)] dark:bg-[--tw-dropdown-background-color] border-1 dark:border-[--tw-dropdown-border] dark:shadow-[--tw-dropdown-box-shadow] rounded-xl will-change-transform">
             <div class="flex items-center justify-between px-5 py-1.5 gap-1.5">
                 <div class="flex items-center gap-2">
                     <img alt="" class="size-9 rounded-full border-2 border-success"
@@ -26,250 +27,23 @@
                     Pro
                 </span>
             </div>
-            <div class="menu-separator">
-            </div>
+            <div class="my-2.5 border-b dark:border-[--tw-dropdown-border]"></div>
             <div class="flex flex-col">
-                <div class="menu-item">
-                    <a class="menu-link" href="html/demo1/public-profile/profiles/default.html">
-                        <span class="menu-icon">
-                            <i class="ki-filled ki-badge">
-                            </i>
+                <div class="flex flex-col p-0 m-0 mb-0.5">
+                    <div class="flex items-center grow mx-2.5 p-2.5 rounded-md cursor-pointer group hover:bg-coal-300">
+                        <span class="flex items-center shrink-0 mr-2.5 group-hover:text-primary">
+                            <i class="ki-filled ki-moon"></i>
                         </span>
-                        <span class="menu-title">
-                            Public Profile
-                        </span>
-                    </a>
-                </div>
-                <div class="menu-item">
-                    <a class="menu-link" href="html/demo1/account/home/user-profile.html">
-                        <span class="menu-icon">
-                            <i class="ki-filled ki-profile-circle">
-                            </i>
-                        </span>
-                        <span class="menu-title">
-                            My Profile
-                        </span>
-                    </a>
-                </div>
-                <div class="menu-item" data-menu-item-offset="-50px, 0" data-menu-item-placement="left-start"
-                    data-menu-item-toggle="dropdown" data-menu-item-trigger="click|lg:hover">
-                    <div class="menu-link">
-                        <span class="menu-icon">
-                            <i class="ki-filled ki-setting-2">
-                            </i>
-                        </span>
-                        <span class="menu-title">
-                            My Account
-                        </span>
-                        <span class="menu-arrow">
-                            <i class="ki-filled ki-right text-3xs">
-                            </i>
-                        </span>
-                    </div>
-                    <div class="menu-dropdown menu-default light:border-gray-300 w-full max-w-[220px]">
-                        <div class="menu-item">
-                            <a class="menu-link" href="html/demo1/account/home/get-started.html">
-                                <span class="menu-icon">
-                                    <i class="ki-filled ki-coffee">
-                                    </i>
-                                </span>
-                                <span class="menu-title">
-                                    Get Started
-                                </span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link" href="html/demo1/account/home/user-profile.html">
-                                <span class="menu-icon">
-                                    <i class="ki-filled ki-some-files">
-                                    </i>
-                                </span>
-                                <span class="menu-title">
-                                    My Profile
-                                </span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link" href="#">
-                                <span class="menu-icon">
-                                    <i class="ki-filled ki-icon">
-                                    </i>
-                                </span>
-                                <span class="menu-title">
-                                    Billing
-                                </span>
-                                <span class="menu-badge" data-tooltip="true" data-tooltip-placement="top">
-                                    <i class="ki-filled ki-information-2 text-md text-gray-500">
-                                    </i>
-                                    <span class="tooltip" data-tooltip-content="true">
-                                        Payment and subscription info
-                                    </span>
-                                </span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link" href="html/demo1/account/security/overview.html">
-                                <span class="menu-icon">
-                                    <i class="ki-filled ki-medal-star">
-                                    </i>
-                                </span>
-                                <span class="menu-title">
-                                    Security
-                                </span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link" href="html/demo1/account/members/teams.html">
-                                <span class="menu-icon">
-                                    <i class="ki-filled ki-setting">
-                                    </i>
-                                </span>
-                                <span class="menu-title">
-                                    Members &amp; Roles
-                                </span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link" href="html/demo1/account/integrations.html">
-                                <span class="menu-icon">
-                                    <i class="ki-filled ki-switch">
-                                    </i>
-                                </span>
-                                <span class="menu-title">
-                                    Integrations
-                                </span>
-                            </a>
-                        </div>
-                        <div class="menu-separator">
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link" href="html/demo1/account/security/overview.html">
-                                <span class="menu-icon">
-                                    <i class="ki-filled ki-shield-tick">
-                                    </i>
-                                </span>
-                                <span class="menu-title">
-                                    Notifications
-                                </span>
-                                <label class="switch switch-sm">
-                                    <input checked="" name="check" type="checkbox" value="1">
-                                    </input>
-                                </label>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="menu-item">
-                    <a class="menu-link" href="https://devs.keenthemes.com">
-                        <span class="menu-icon">
-                            <i class="ki-filled ki-message-programming">
-                            </i>
-                        </span>
-                        <span class="menu-title">
-                            Dev Forum
-                        </span>
-                    </a>
-                </div>
-                <div class="menu-item" data-menu-item-offset="-10px, 0" data-menu-item-placement="left-start"
-                    data-menu-item-toggle="dropdown" data-menu-item-trigger="click|lg:hover">
-                    <div class="menu-link">
-                        <span class="menu-icon">
-                            <i class="ki-filled ki-icon">
-                            </i>
-                        </span>
-                        <span class="menu-title">
-                            Language
-                        </span>
-                        <div
-                            class="flex items-center gap-1.5 rounded-md border border-gray-300 text-gray-600 p-1.5 text-2xs font-medium shrink-0">
-                            English
-                            <img alt="" class="inline-block size-3.5 rounded-full"
-                                src="{{ asset('assets/media/flags/united-states.svg') }}" />
-                        </div>
-                    </div>
-                    <div class="menu-dropdown menu-default light:border-gray-300 w-full max-w-[170px]">
-                        <div class="menu-item active">
-                            <a class="menu-link h-10" href="#">
-                                <span class="menu-icon">
-                                    <img alt="" class="inline-block size-4 rounded-full"
-                                        src="{{ asset('assets/media/flags/united-states.svg') }}" />
-                                </span>
-                                <span class="menu-title">
-                                    English
-                                </span>
-                                <span class="menu-badge">
-                                    <i class="ki-solid ki-check-circle text-success text-base">
-                                    </i>
-                                </span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link h-10" href="#">
-                                <span class="menu-icon">
-                                    <img alt="" class="inline-block size-4 rounded-full"
-                                        src="{{ asset('assets/media/flags/spain.svg') }}" />
-                                </span>
-                                <span class="menu-title">
-                                    Spanish
-                                </span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link h-10" href="#">
-                                <span class="menu-icon">
-                                    <img alt="" class="inline-block size-4 rounded-full"
-                                        src="{{ asset('assets/media/flags/germany.svg') }}" />
-                                </span>
-                                <span class="menu-title">
-                                    German
-                                </span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link h-10" href="#">
-                                <span class="menu-icon">
-                                    <img alt="" class="inline-block size-4 rounded-full"
-                                        src="{{ asset('assets/media/flags/japan.svg') }}" />
-                                </span>
-                                <span class="menu-title">
-                                    Japanese
-                                </span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link h-10" href="#">
-                                <span class="menu-icon">
-                                    <img alt="" class="inline-block size-4 rounded-full"
-                                        src="{{ asset('assets/media/flags/france.svg') }}" />
-                                </span>
-                                <span class="menu-title">
-                                    French
-                                </span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="menu-separator">
-            </div>
-            <div class="flex flex-col">
-                <div class="menu-item mb-0.5">
-                    <div class="menu-link">
-                        <span class="menu-icon">
-                            <i class="ki-filled ki-moon">
-                            </i>
-                        </span>
-                        <span class="menu-title">
+                        <span
+                            class="flex items-center grow text-gray-800 text-2sm font-medium group-hover:text-gray-900">
                             Dark Mode
                         </span>
-                        <label class="switch switch-sm">
-                            <input data-theme-state="dark" data-theme-toggle="true" name="check" type="checkbox"
-                                value="1">
-                            </input>
+                        <label class="flex items-center cursor-pointer gap-2.5">
+                            <x-topbar.dark />
                         </label>
                     </div>
                 </div>
-                <div class="menu-item px-4 py-1.5">
+                <div class="flex flex-col m-0 px-4 py-1.5">
                     <a class="btn btn-sm btn-light justify-center"
                         href="html/demo1/authentication/classic/sign-in.html">
                         Log out
