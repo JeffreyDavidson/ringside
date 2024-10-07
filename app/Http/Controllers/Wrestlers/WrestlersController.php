a5d66a9146b1c56c918331919850d8109d118ea5
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-07 12:54:07 -0400

    Remove unneeded files and functions

--- app/Http/Controllers/Wrestlers/WrestlersController.php
+++ app/Http/Controllers/Wrestlers/WrestlersController.php
@@ -4,16 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Wrestlers;
 
-use App\Actions\Wrestlers\CreateAction;
-use App\Actions\Wrestlers\DeleteAction;
-use App\Actions\Wrestlers\UpdateAction;
-use App\Data\WrestlerData;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Wrestlers\StoreRequest;
-use App\Http\Requests\Wrestlers\UpdateRequest;
 use App\Models\Wrestler;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Gate;
 
 class WrestlersController extends Controller
@@ -29,28 +22,6 @@ class WrestlersController extends Controller
     }
 
     /**
-     * Show the form for creating a new wrestler.
-     */
-    public function create(Wrestler $wrestler): View
-    {
-        Gate::authorize('create', Wrestler::class);
-
-        return view('wrestlers.create', [
-            'wrestler' => $wrestler,
-        ]);
-    }
-
-    /**
-     * Create a new wrestler.
-     */
-    public function store(StoreRequest $request): RedirectResponse
-    {
-        CreateAction::run(WrestlerData::fromStoreRequest($request));
-
-        return to_route('wrestlers.index');
-    }
-
-    /**
      * Show the profile of a wrestler.
      */
     public function show(Wrestler $wrestler): View
@@ -60,39 +31,5 @@ class WrestlersController extends Controller
         return view('wrestlers.show', [
             'wrestler' => $wrestler,
         ]);
-    }
-
-    /**
-     * Show the form for editing a wrestler.
-     */
-    public function edit(Wrestler $wrestler): View
-    {
-        Gate::authorize('update', $wrestler);
-
-        return view('wrestlers.edit', [
-            'wrestler' => $wrestler,
-        ]);
-    }
-
-    /**
-     * Update a given wrestler.
-     */
-    public function update(UpdateRequest $request, Wrestler $wrestler): RedirectResponse
-    {
-        UpdateAction::run($wrestler, WrestlerData::fromUpdateRequest($request));
-
-        return to_route('wrestlers.index');
-    }
-
-    /**
-     * Delete a wrestler.
-     */
-    public function destroy(Wrestler $wrestler): RedirectResponse
-    {
-        Gate::authorize('delete', $wrestler);
-
-        DeleteAction::run($wrestler);
-
-        return to_route('wrestlers.index');
     }
 }
