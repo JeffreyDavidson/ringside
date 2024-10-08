8543557e21e59d7fe951dd3bb3d4dc8a7bc1c540
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-08 15:22:54 -0400

    Change default dark header color to tailwind color

--- resources/views/components/layouts/partials/header.blade.php
+++ resources/views/components/layouts/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header
-    class="header fixed top-0 z-10 left-0 right-0 flex items-stretch shrink-0 bg-[--tw-page-bg] dark:bg-[--tw-page-bg-dark]"
+    class="header fixed top-0 z-10 left-0 right-0 flex items-stretch shrink-0 bg-[--tw-page-bg] dark:bg-coal-200"
     id="header">
     <!-- Container -->
     <div class="container-fixed flex justify-end lg:gap-4" id="header_container">
