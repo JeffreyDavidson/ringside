7c4783ae9801821ab190b7da42efa96b5d63dbcb
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-10 10:17:19 -0400

    Update blade components

---
+++ resources/views/components/form/inputs/password.blade.php
@@ -0,0 +1,2 @@
+<input type="password"
+    {{ $attributes->merge(['name' => 'password', 'placeholder' => 'Enter '. __('user.password.placeholder'), 'class' => 'block w-full appearance-none shadow-none outline-none font-medium text-[.8125rem] leading-4 bg-light rounded-md h-10 px-3 border border-solid border-gray-300 text-gray-700 hover:border-gray-400']) }}"/>
--- resources/views/components/form/inputs/text.blade.php
+++ resources/views/components/form/inputs/text.blade.php
@@ -1,2 +1,2 @@
 <input type="text"
-    {{ $attributes->merge(['class' => 'block w-full appearance-none shadow-none font-medium text-2sm leading-4 border border-1 border-gray-300 rounded-md h-10 px-3 text-gray-700 bg-light hover:border-gray-400']) }}/>
+    {{ $attributes->merge(['class' => 'block w-full appearance-none shadow-none outline-none font-medium text-[.8125rem] leading-4 bg-light rounded-md h-10 px-3 border border-solid border-gray-300 text-gray-700 hover:border-gray-400']) }}/>
