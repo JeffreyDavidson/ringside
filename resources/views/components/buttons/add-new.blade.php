d8a3d9a830bad97e10fce7541d7d544e32d95824
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-12 19:12:36 -0400

    Add  correct variable literal string

--- resources/views/components/buttons/add-new.blade.php
+++ resources/views/components/buttons/add-new.blade.php
@@ -2,6 +2,6 @@
     'component' => '',
 ])
 
-<button class="inline-flex items-center cursor-pointer rounded-md border border-solid border-transparent outline-none h-8 px-3 font-medium text-xs text-white gap-[.275rem] dark:bg-primary dark:hover:bg-primary-active hover:shadow-none" @click="$dispatch('openModal', { component: {{ $component }} })">
+<button class="inline-flex items-center cursor-pointer rounded-md border border-solid border-transparent outline-none h-8 px-3 font-medium text-xs text-white gap-[.275rem] dark:bg-primary dark:hover:bg-primary-active hover:shadow-none" @click="$dispatch('openModal', { component: '{{ $component }}' })">
     {{ $slot }}
 </button>
