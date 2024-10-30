a5655c67c3769e184f934d22ddd88af9adc715fc
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-30 08:40:28 -0400

    Add stable activation model

---
+++ app/Models/StableActivation.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class StableActivation extends Model
+{
+    /** @use HasFactory<\Database\Factories\StableActivationFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'stable_id',
+        'started_at',
+        'ended_at',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'started_at' => 'datetime',
+            'ended_at' => 'datetime',
+        ];
+    }
+
+    /**
+     * @return BelongsTo<Stable, $this>
+     */
+    public function stable(): BelongsTo
+    {
+        return $this->belongsTo(Stable::class);
+    }
+}
