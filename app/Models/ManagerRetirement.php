5ee8e41cf7179a817da88491df95c3c2ac9ff1f9
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-30 00:07:02 -0400

    Add model retirements models

---
+++ app/Models/ManagerRetirement.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Manager;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ManagerRetirement extends Model
+{
+    /** @use HasFactory<\Database\Factories\ManagerRetirementFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'manager_id',
+        'started_at',
+        'ended_at',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'started_at' => 'datetime',
+            'ended_at' => 'datetime',
+        ];
+    }
+
+    /**
+     * @return BelongsTo<Manager, $this>
+     */
+    public function manager(): BelongsTo
+    {
+        return $this->belongsTo(Manager::class);
+    }
+}
