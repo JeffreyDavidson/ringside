c2fe3a8679e52b41fc415545d6ed4dab30bf0564
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-10-31 13:43:11 -0400

    Remove unused imports

--- app/Models/Concerns/HasManagers.php
+++ app/Models/Concerns/HasManagers.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace App\Models\Concerns;
 
-use App\Models\Managable;
 use App\Models\Manager;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 trait HasManagers
 {
