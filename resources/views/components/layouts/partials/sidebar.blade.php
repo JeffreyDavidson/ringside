f1ffbb5962723f893d0648501a308a48591245cd
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-11-02 09:22:41 -0400

    Update locations of images

--- resources/views/components/layouts/partials/sidebar.blade.php
+++ resources/views/components/layouts/partials/sidebar.blade.php
@@ -4,13 +4,13 @@
     <div class="sidebar-header hidden lg:flex items-center relative justify-between px-3 lg:px-6 shrink-0"
         id="sidebar_header">
         <a class="dark:hidden" href="{{ route('dashboard') }}">
-            <img class="default-logo min-h-[22px] max-w-none" src="{{ asset('assets/media/app/default-logo.svg') }}" />
-            <img class="small-logo min-h-[22px] max-w-none" src="{{ asset('assets/media/app/mini-logo.svg') }}" />
+            <img class="default-logo min-h-[22px] max-w-none" src="{{ Vite::image('app/default-logo.svg') }}" />
+            <img class="small-logo min-h-[22px] max-w-none" src="{{ Vite::image('app/mini-logo.svg') }}" />
         </a>
         <a class="hidden dark:block" href="{{ route('dashboard') }}">
             <img class="default-logo min-h-[22px] max-w-none"
-                src="{{ asset('assets/media/app/default-logo-dark.svg') }}" />
-            <img class="small-logo min-h-[22px] max-w-none" src="{{ asset('assets/media/app/mini-logo.svg') }}" />
+                src="{{ Vite::image('app/default-logo-dark.svg') }}" />
+            <img class="small-logo min-h-[22px] max-w-none" src="{{ Vite::image('app/mini-logo.svg') }}" />
         </a>
         <button
             class="btn btn-icon btn-icon-md size-[30px] rounded-lg border border-gray-200 dark:border-gray-300 bg-light text-gray-500 hover:text-gray-700 toggle absolute left-full top-2/4 -translate-x-2/4 -translate-y-2/4"
