955285e4a7850dfaaeb997a2095b61002b77fbc7
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-11-10 08:24:14 -0500

    Apply new component to sidebar header

--- resources/views/components/sidebar/header.blade.php
+++ resources/views/components/sidebar/header.blade.php
@@ -3,12 +3,7 @@
         <img class="min-h-[22px] max-w-none hidden lg:block" src="{{ Vite::image('app/default-logo.svg') }}" />
         <img class="min-h-[22px] max-w-none lg:hidden" src="{{ Vite::image('app/mini-logo.svg') }}" />
     </a>
-    <button
-        class="btn btn-icon btn-icon-md size-[30px] rounded-lg border border-gray-200 bg-light text-gray-500 hover:text-gray-700 toggle absolute left-full top-2/4 -translate-x-2/4 -translate-y-2/4">
+    <x-button withIcon class="size-[30px] rounded-lg border-gray-200 bg-light text-gray-500 hover:text-gray-700 absolute start-full top-2/4 -translate-x-2/4 -translate-y-2/4">
         <i class="ki-filled ki-black-left-line toggle-active:rotate-180 transition-all duration-300"></i>
-    </button>
-    <button
-        class="btn btn-icon btn-icon-md size-[30px] rounded-lg border border-gray-200 bg-light text-gray-500 hover:text-gray-700 toggle absolute start-full top-2/4 -translate-x-2/4 -translate-y-2/4">
-        <i class="ki-filled ki-black-left-line toggle-active:rotate-180 transition-all duration-300"></i>
-    </button>
+    </x-button>
 </div>
