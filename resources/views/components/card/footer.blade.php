3f7a4093bbd448f1782ecaf8be40ed3f77e04ea6
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-11-12 12:41:36 -0500

    Remove the border on all sides but top

--- resources/views/components/card/footer.blade.php
+++ resources/views/components/card/footer.blade.php
@@ -1,4 +1,3 @@
-<div
-    {{ $attributes->merge(['class' => 'flex items-center justify-between border border-t border-gray-200 ps-5 pe-5 py-3']) }}>
+<div {{ $attributes->merge(['class' => 'flex items-center justify-between border-t border-gray-200 ps-5 pe-5 py-3']) }}>
     {{ $slot }}
 </div>
