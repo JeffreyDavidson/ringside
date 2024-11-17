d73f86f5649937c32c97569150c2ed0ded8b3bc3
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-11-16 22:17:53 -0500

    Add pivot relationships

--- app/Models/WrestlerManager.php
+++ app/Models/WrestlerManager.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Models\Manager;
+use App\Models\Wrestler;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class WrestlerManager extends Pivot
@@ -19,4 +22,20 @@ class WrestlerManager extends Pivot
         'hired_at' => 'datetime',
         'left_at' => 'datetime',
     ];
+
+    /**
+     * @return BelongsTo<Manager, WrestlerManager>
+     */
+    public function manager(): BelongsTo
+    {
+        return $this->belongsTo(Manager::class);
+    }
+
+    /**
+     * @return BelongsTo<Wrestler, WrestlerManager>
+     */
+    public function wrestler(): BelongsTo
+    {
+        return $this->belongsTo(Wrestler::class);
+    }
 }
