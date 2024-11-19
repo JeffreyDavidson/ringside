fe1ca3ed27c6cc6d1c372c5ff9c06d9a9ca3adad
JeffreyDavidson <JeffreyDavidson@users.noreply.github.com>
2024-11-19 19:18:51 +0000

    Fix styling

--- app/Livewire/Concerns/BaseTableTrait.php
+++ app/Livewire/Concerns/BaseTableTrait.php
@@ -47,73 +47,73 @@ trait BaseTableTrait
             'default-colors' => false,
             'class' => 'flex appearance-none shadow-none outline-none bg-no-repeat bg-[right_.675rem_center] bg-[#fcfcfc] rounded-md border border-solid border-gray-300 font-medium text-xs h-8 ps-2.5 pe-2.5 bg-[length:14px_10px] w-16',
         ])
-        ->setSearchFieldAttributes([
-            'default' => false,
-            'default-styling' => false,
-            'default-colors' => false,
-            'class' => 'grow bg-transparent border border-transparent text-inherit appearance-none outline-none opacity-100 active:shadow-none active:text-gray-700 focus:border-primary text-xs',
-        ])
-        ->setTableWrapperAttributes([
-            'default' => false,
-            'default-styling' => false,
-            'default-colors' => false,
-            'class' => 'scrollable-x-auto',
-        ])
-        ->setTableAttributes([
-            'default' => false,
-            'default-styling' => false,
-            'default-colors' => false,
-            'class' => 'table table-auto w-full caption-bottom border-collapse text-left text-gray-700 font-medium text-sm border-0',
-        ])
-        ->setTheadAttributes([
-            'default' => false,
-            'default-styling' => false,
-            'default-colors' => false,
-            'class' => '',
-        ])
-        ->setThAttributes(function () {
-            return [
+            ->setSearchFieldAttributes([
                 'default' => false,
                 'default-styling' => false,
                 'default-colors' => false,
-                'class' => 'bg-[#fcfcfc] text-gray-600 font-medium text-[.8125rem] leading-[1.125rem] align-middle py-2.5 ps-4 pe-4 border-b border-gray-200 border-e border-e-solid border-e-gray-200',
-            ];
-        })
-        ->setThSortButtonAttributes(function () {
-            return [
+                'class' => 'grow bg-transparent border border-transparent text-inherit appearance-none outline-none opacity-100 active:shadow-none active:text-gray-700 focus:border-primary text-xs',
+            ])
+            ->setTableWrapperAttributes([
+                'default' => false,
+                'default-styling' => false,
+                'default-colors' => false,
+                'class' => 'scrollable-x-auto',
+            ])
+            ->setTableAttributes([
+                'default' => false,
+                'default-styling' => false,
+                'default-colors' => false,
+                'class' => 'table table-auto w-full caption-bottom border-collapse text-left text-gray-700 font-medium text-sm border-0',
+            ])
+            ->setTheadAttributes([
                 'default' => false,
                 'default-styling' => false,
                 'default-colors' => false,
                 'class' => '',
-            ];
-        })
-        ->setTbodyAttributes([
-            'default' => false,
-            'default-styling' => false,
-            'default-colors' => false,
-            'class' => '',
-        ])
-        ->setTrAttributes(function () {
-            return [
+            ])
+            ->setThAttributes(function () {
+                return [
+                    'default' => false,
+                    'default-styling' => false,
+                    'default-colors' => false,
+                    'class' => 'bg-[#fcfcfc] text-gray-600 font-medium text-[.8125rem] leading-[1.125rem] align-middle py-2.5 ps-4 pe-4 border-b border-gray-200 border-e border-e-solid border-e-gray-200',
+                ];
+            })
+            ->setThSortButtonAttributes(function () {
+                return [
+                    'default' => false,
+                    'default-styling' => false,
+                    'default-colors' => false,
+                    'class' => '',
+                ];
+            })
+            ->setTbodyAttributes([
                 'default' => false,
                 'default-styling' => false,
                 'default-colors' => false,
                 'class' => '',
-            ];
-        })
-        ->setTdAttributes(function () {
-            return [
+            ])
+            ->setTrAttributes(function () {
+                return [
+                    'default' => false,
+                    'default-styling' => false,
+                    'default-colors' => false,
+                    'class' => '',
+                ];
+            })
+            ->setTdAttributes(function () {
+                return [
+                    'default' => false,
+                    'default-styling' => false,
+                    'default-colors' => false,
+                    'class' => 'py-3 pe-px border-b border-solid border-gray-200 border-e-solid border-e-gray-200',
+                ];
+            })
+            ->setPaginationWrapperAttributes([
                 'default' => false,
                 'default-styling' => false,
                 'default-colors' => false,
-                'class' => 'py-3 pe-px border-b border-solid border-gray-200 border-e-solid border-e-gray-200',
-            ];
-        })
-        ->setPaginationWrapperAttributes([
-            'default' => false,
-            'default-styling' => false,
-            'default-colors' => false,
-            'class' => 'inline-flex items-center gap-1',
-        ]);
+                'class' => 'inline-flex items-center gap-1',
+            ]);
     }
 }
