82f2501ce3e7290cf0f2276730afed5ec6d01f9f
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-11-20 10:50:32 -0500

    Add temp view

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ require __DIR__.'/auth.php';
 
 Route::redirect('/', 'login');
 
+Route::view('testing', 'wrestlers.index');
+
 Route::middleware(['middleware' => 'auth'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
