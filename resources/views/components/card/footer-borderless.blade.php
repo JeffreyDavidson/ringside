118cbcaee17700471712d6b3e0d876fbca2005a4
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-11-21 18:44:02 -0500

    Refactor to fix border issue

--- resources/views/components/card/footer-borderless.blade.php
+++ resources/views/components/card/footer-borderless.blade.php
@@ -1,12 +1,10 @@
 @aware(['inGrid' => false])
-@props(['border' => true])
 
 <div
     {{ $attributes->class([
         'flex items-center justify-between py-3',
         'ps-7.5 pe-7.5' => !$inGrid,
         'ps-5 pe-5' => $inGrid,
-        'border-t border-solid border-gray-200' => $border,
     ]) }}>
     {{ $slot }}
 </div>
