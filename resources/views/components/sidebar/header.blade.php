954aa7e0bcc4ac6611b1cfa8e07c2886564955f9
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-12-14 10:46:35 -0500

    Remove duplicated button

--- resources/views/components/sidebar/header.blade.php
+++ resources/views/components/sidebar/header.blade.php
@@ -7,8 +7,4 @@
         class="btn btn-icon btn-icon-md size-[30px] rounded-lg border border-gray-200 bg-light text-gray-500 hover:text-gray-700 toggle absolute left-full top-2/4 -translate-x-2/4 -translate-y-2/4">
         <i class="ki-filled ki-black-left-line toggle-active:rotate-180 transition-all duration-300"></i>
     </button>
-    <button
-        class="btn btn-icon btn-icon-md size-[30px] rounded-lg border border-gray-200 bg-light text-gray-500 hover:text-gray-700 toggle absolute start-full top-2/4 -translate-x-2/4 -translate-y-2/4">
-        <i class="ki-filled ki-black-left-line toggle-active:rotate-180 transition-all duration-300"></i>
-    </button>
 </div>
