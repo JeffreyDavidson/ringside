9bffd899ed410197ece4e704a6737f53e9200242
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-12-17 21:16:52 -0500

    More fixes

--- resources/views/components/sidebar/index.blade.php
+++ resources/views/components/sidebar/index.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-light border-e border-e-gray-200 z-20 hidden flex-col items-stretch shrink-0 hover:transition hover:ease hover:duration-300 hover:w-[280px] group lg:flex" :class="!sidebarIsOpen ? 'lg:w-[80px]' : 'lg:w-[280px]'" {{ $attributes }}">
+<div class="bg-light border-e border-e-gray-200 z-20 hidden flex-col items-stretch shrink-0 x-transition.duration.3000ms group lg:flex" :class="!sidebarIsOpen ? 'lg:w-[80px]' : 'lg:w-[280px]'" :class="sidebarIsOpen ? 'w-[280px]' : 'w-[80px]'" {{ $attributes }}">
     <x-sidebar.header/>
     <x-sidebar.content/>
 </div>
