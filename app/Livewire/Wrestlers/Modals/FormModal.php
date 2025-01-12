7e8b0af952c9332d91349d22a6fb25689172a1e8
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-01-12 10:45:01 -0500

    WIP

--- app/Livewire/Wrestlers/Modals/FormModal.php
+++ app/Livewire/Wrestlers/Modals/FormModal.php
@@ -7,6 +7,7 @@ namespace App\Livewire\Wrestlers\Modals;
 use App\Livewire\Concerns\BaseModal;
 use App\Livewire\Wrestlers\WrestlerForm;
 use App\Models\Wrestler;
+use Illuminate\Support\Str;
 
 class FormModal extends BaseModal
 {
@@ -17,4 +18,15 @@ class FormModal extends BaseModal
     protected string $modalFormPath = 'wrestlers.modals.form-modal';
 
     public WrestlerForm $modelForm;
+
+    public function fillDummyFields()
+    {
+        $this->modelForm->name = Str::title(fake()->words(2, true));
+        $this->modelForm->hometown = fake()->city().', '.fake()->state();
+        $this->modelForm->height_feet = fake()->numberBetween(5, 8);
+        $this->modelForm->height_inches = fake()->numberBetween(0, 11);
+        $this->modelForm->weight = fake()->numberBetween(180, 400);
+        $this->modelForm->signature_move = Str::title(fake()->optional(0.8)->words(3, true));
+        $this->modelForm->start_date = fake()->optional(0.8)->dateTimeBetween('now', '+3 month');
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -72,12 +72,20 @@ export default {
                     clarity: 'rgba(246, 177, 0, 0.20)',
                     inverse: '#ffffff'
                 },
+                info: {
+                    default: '#7239EA',
+                    active: '#5014D0',
+                    light: '#F8F5FF',
+                    clarity: 'rgba(114, 57, 234, 0.20)',
+                    inverse: '#ffffff'
+                },
             },
             boxShadows: {
                 default: '0px 4px 12px 0px rgba(0, 0, 0, 0.09)',
                 light: '0px 3px 4px 0px rgba(0, 0, 0, 0.03)',
                 primary: '0px 4px 12px 0px rgba(40, 132, 239, 0.35)',
                 danger: '0px 4px 12px 0px rgba(241, 65, 108, 0.35)',
+                info: '0px 4px 12px 0px rgba(114, 57, 234, 0.35)',
                 warning: '0px 4px 12px 0px rgba(246, 192, 0, 0.35)',
             },
             fontSize: {
