d9a434b599d5b103633434a754bbf68c899aec8e
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-01-12 14:44:28 -0500

    wip

--- resources/views/components/form/footer.blade.php
+++ resources/views/components/form/footer.blade.php
@@ -1,7 +1,11 @@
-<div class="flex gap-4">
-    @env('local')
-    <x-buttons.info wire:click="fillDummyFields">Auto Fill</x-buttons.info>
-    @endenv
-    <x-buttons.light wire:click="clear">Clear</x-buttons.light>
-    <x-buttons.primary wire:click="save">Save</x-buttons.primary>
+<div class="flex justify-between flex-1">
+    <div>
+        @env('local')
+        <x-buttons.info wire:click="fillDummyFields">Auto Fill</x-buttons.info>
+        @endenv
+    </div>
+    <div>
+        <x-buttons.light wire:click="clear">Clear</x-buttons.light>
+        <x-buttons.primary wire:click="save">Save</x-buttons.primary>
+    </div>
 </div>
