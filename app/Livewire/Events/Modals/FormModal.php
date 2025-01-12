2906be93693d21d409c364d7e090b675235b29de
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-01-12 15:30:29 -0500

    Update filling dummy data

--- app/Livewire/Events/Modals/FormModal.php
+++ app/Livewire/Events/Modals/FormModal.php
@@ -7,6 +7,7 @@ namespace App\Livewire\Events\Modals;
 use App\Livewire\Concerns\BaseModal;
 use App\Livewire\Events\EventForm;
 use App\Models\Event;
+use Illuminate\Support\Str;
 
 class FormModal extends BaseModal
 {
@@ -17,4 +18,12 @@ class FormModal extends BaseModal
     protected string $modalFormPath = 'events.modals.form-modal';
 
     public EventForm $modelForm;
+
+    public function fillDummyFields()
+    {
+        $this->modelForm->name = Str::of(fake()->words(2, true))->title()->value();
+        $this->modelForm->date = fake()->city();
+        $this->modelForm->venue = fake()->streetAddress();
+        $this->modelForm->preview = fake()->paragraphs(4, true);
+    }
 }
