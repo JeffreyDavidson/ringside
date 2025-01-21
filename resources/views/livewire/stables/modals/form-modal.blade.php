2b52ed74b419567aae77089b5677fff9a530b8f3
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-01-21 15:08:35 -0500

    wip

--- resources/views/livewire/stables/modals/form-modal.blade.php
+++ resources/views/livewire/stables/modals/form-modal.blade.php
@@ -6,4 +6,23 @@
     <x-form-modal.modal-input>
         <x-form.inputs.date label="{{ __('activations.started_at') }}" wire:model="modelForm.start_date" />
     </x-form-modal.modal-input>
+
+    <x-form-modal.modal-input>
+        <x-form.inputs.date label="{{ __('activations.started_at') }}" wire:model="modelForm.start_date" />
+    </x-form-modal.modal-input>
+
+    <x-form-modal.modal-input>
+        <x-form.inputs.select label="{{ __('core.wrestlers') }}" wire:model="modelForm.wrestlers" :options="$this->getWrestlers"
+            selected="modelForm.wrestlers" />
+    </x-form-modal.modal-input>
+
+    <x-form-modal.modal-input>
+        <x-form.inputs.select label="{{ __('core.tag-teams') }}" wire:model="modelForm.tag_teams" :options="$this->getTagTeams"
+            selected="modelForm.tagTeams" />
+    </x-form-modal.modal-input>
+
+    <x-form-modal.modal-input>
+        <x-form.inputs.select label="{{ __('core.managers') }}" wire:model="modelForm.managers" :options="$this->getManagers"
+            selected="modelForm.managers" />
+    </x-form-modal.modal-input>
 </x-form-modal>
