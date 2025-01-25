354971b2e71eca7b5f409aa3d12dfbcc4671a0cc
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-01-25 12:43:45 -0500

    Remove request specific methods from data dtos

--- app/Data/ManagerData.php
+++ app/Data/ManagerData.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Data;
 
-use App\Http\Requests\Managers\StoreRequest;
-use App\Http\Requests\Managers\UpdateRequest;
 use Illuminate\Support\Carbon;
 
 readonly class ManagerData
@@ -18,28 +16,4 @@ readonly class ManagerData
         public string $last_name,
         public ?Carbon $start_date,
     ) {}
-
-    /**
-     * Create a DTO from the store request.
-     */
-    public static function fromStoreRequest(StoreRequest $request): self
-    {
-        return new self(
-            $request->string('first_name')->value(),
-            $request->string('last_name')->value(),
-            $request->date('start_date')
-        );
-    }
-
-    /**
-     * Create a DTO from the update request.
-     */
-    public static function fromUpdateRequest(UpdateRequest $request): self
-    {
-        return new self(
-            $request->string('first_name')->value(),
-            $request->string('last_name')->value(),
-            $request->date('start_date')
-        );
-    }
 }
--- app/Data/RefereeData.php
+++ app/Data/RefereeData.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Data;
 
-use App\Http\Requests\Referees\StoreRequest;
-use App\Http\Requests\Referees\UpdateRequest;
 use Illuminate\Support\Carbon;
 
 readonly class RefereeData
@@ -18,28 +16,4 @@ readonly class RefereeData
         public string $last_name,
         public ?Carbon $start_date,
     ) {}
-
-    /**
-     * Create a DTO from the store request.
-     */
-    public static function fromStoreRequest(StoreRequest $request): self
-    {
-        return new self(
-            $request->string('first_name')->value(),
-            $request->string('last_name')->value(),
-            $request->date('start_date')
-        );
-    }
-
-    /**
-     * Create a DTO from the update request.
-     */
-    public static function fromUpdateRequest(UpdateRequest $request): self
-    {
-        return new self(
-            $request->string('first_name')->value(),
-            $request->string('last_name')->value(),
-            $request->date('start_date')
-        );
-    }
 }
