1ac6596b4d52c41e92d62fd3ff5bf4253446866e
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-01-29 13:12:15 -0500

    wip

--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -1,29 +1,27 @@
 <div id="sidebar-content" class="flex grow shrink-0 py-5 pe-2" :class="!sidebarIsOpen ? 'lg:overflow-hidden' : ''">
-    <div class="relative scrollbar-thin scrollbar-transparent overflow-y-scroll grow shrink-0 flex ps-2 lg:ps-5 pe-1 lg:pe-3">
+    <div
+        class="relative scrollbar-thin scrollbar-transparent overflow-y-scroll grow shrink-0 flex ps-2 lg:ps-5 pe-1 lg:pe-3">
         <!-- Sidebar Menu -->
         <x-sidebar.menu class="flex flex-col grow gap-0.5">
             <x-sidebar.menu-item>
                 <x-sidebar.menu-label>
                     <x-sidebar.menu-icon icon="ki-element-11" />
-                    <x-sidebar.menu-link
-                        x-show="sidebarIsOpen" x-cloak
-                        href="{{ route('dashboard') }}"
-                        :isCurrent="request()->routeIs('dashboard')">
+                    <x-sidebar.menu-link x-show="sidebarIsOpen" x-cloak href="{{ route('dashboard') }}" :isCurrent="request()->routeIs('dashboard')">
                         Dashboard
                     </x-sidebar.menu-link>
                 </x-sidebar.menu-label>
             </x-sidebar.menu-item>
             <x-sidebar.menu-heading>User</x-sidebar.menu-heading>
             <div x-data="{
-                open: @json(request()->is('roster/*')),
+                sidebarIsOpen: @json(request()->is('roster/*')),
                 toggle() {
-                    this.open = !this.open
+                    this.sidebarIsOpen = !this.sidebarIsOpen
                 }
             }">
                 <x-sidebar.menu-label @click="toggle">
                     <x-sidebar.menu-icon icon="ki-people" />
                     <x-sidebar.menu-title x-show="sidebarIsOpen" x-cloak>Roster</x-sidebar.menu-title>
-                    <x-sidebar.menu-accordian-icons x-show="sidebarIsOpen" x-cloak/>
+                    <x-sidebar.menu-accordian-icons x-show="sidebarIsOpen" x-cloak />
                 </x-sidebar.menu-label>
                 <x-sidebar.menu-accordian x-show="open">
                     <x-sidebar.accordian-link href="{{ route('wrestlers.index') }}"
@@ -41,10 +39,7 @@
             <x-sidebar.menu-item>
                 <x-sidebar.menu-label>
                     <x-sidebar.menu-icon icon="ki-cup" />
-                    <x-sidebar.menu-link
-                        x-show="sidebarIsOpen" x-cloak
-                        :href="route('titles.index')"
-                        :isCurrent="request()->routeIs('titles.*')">
+                    <x-sidebar.menu-link x-show="sidebarIsOpen" x-cloak :href="route('titles.index')" :isCurrent="request()->routeIs('titles.*')">
                         Titles
                     </x-sidebar.menu-link>
                 </x-sidebar.menu-label>
@@ -52,10 +47,7 @@
             <x-sidebar.menu-item>
                 <x-sidebar.menu-label>
                     <x-sidebar.menu-icon icon="ki-home-3" />
-                    <x-sidebar.menu-link
-                        x-show="sidebarIsOpen" x-cloak
-                        :href="route('venues.index')"
-                        :isCurrent="request()->routeIs('venues.*')">
+                    <x-sidebar.menu-link x-show="sidebarIsOpen" x-cloak :href="route('venues.index')" :isCurrent="request()->routeIs('venues.*')">
                         Venues
                     </x-sidebar.menu-link>
                 </x-sidebar.menu-label>
@@ -63,10 +55,7 @@
             <x-sidebar.menu-item>
                 <x-sidebar.menu-label>
                     <x-sidebar.menu-icon icon="ki-calendar" />
-                    <x-sidebar.menu-link
-                        x-show="sidebarIsOpen" x-cloak
-                        :href="route('events.index')"
-                        :isCurrent="request()->routeIs('events.*')">
+                    <x-sidebar.menu-link x-show="sidebarIsOpen" x-cloak :href="route('events.index')" :isCurrent="request()->routeIs('events.*')">
                         Events
                     </x-sidebar.menu-link>
                 </x-sidebar.menu-label>
