430dbb8307e7cc6554be19e0f113aa7dc9b7f3e2
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-01-31 09:17:17 -0500

    Add return type for controller invoke

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
+
 class DashboardController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke()
+    public function __invoke(): View
     {
         return view('dashboard');
     }
